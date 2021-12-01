0a4582b6f44f0dd7e12f15c676cb9daf116fb8d4
pem <pierre-etienne.moreau@loria.fr>
2021-12-01 09:17:25 +0100

    first version

--- src/day01.rs
+++ src/day01.rs
@@ -1,46 +1,22 @@
-use itertools::Itertools;
-
-fn values(input: String) -> Vec<u32> {
+// use itertools::Itertools;
+fn values(input: String) -> Vec<i32> {
     input.lines().map(|line| line.parse().unwrap()).collect()
+}
+
+pub fn increases(values: &Vec<i32>) -> usize {
+    let iter = values.windows(2);
+    let diff = iter.map(|pair| pair[1] - pair[0]);
+    return diff.filter(|&x| x > 0).count();
 }
 
 pub fn part1(input: String) {
     let values = values(input);
-    for (i, value1) in values.iter().enumerate() {
-        for value2 in values.iter().skip(i) {
-            if value1 + value2 == 2020 {
-                println!("{} {} product = {}", value1, value2, value1 * value2);
-            }
-        }
-    }
+    println!("increases = {}", increases(&values));
 }
 
 pub fn part2(input: String) {
     let values = values(input);
-    let (a, b, c) = values
-        .into_iter()
-        .tuple_combinations()
-        .find(|(a, b, c)| a + b + c == 2020)
-        .expect("no tuple of length 3 had a sum of 2020");
-    println!("{} {} {} product = {}", a, b, c, a * b * c);
-}
-
-pub fn part3(input: String) {
-    let values = values(input);
-
-    for (i, value1) in values.iter().enumerate() {
-        for (j, value2) in values.iter().skip(i).enumerate() {
-            for value3 in values.iter().skip(i + j) {
-                if value1 + value2 + value3 == 2020 {
-                    println!(
-                        "{} {} {} product = {}",
-                        value1,
-                        value2,
-                        value3,
-                        value1 * value2 * value3
-                    );
-                }
-            }
-        }
-    }
+    let sums = values.windows(3).map(|pair| pair[0] + pair[1] + pair[2]);
+    let vsums = sums.collect::<Vec<i32>>();
+    println!("increases = {}", increases(&vsums));
 }

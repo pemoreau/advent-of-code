ced185c657ab2afaf7cb646f049a01c117609834
pem <pierre-etienne.moreau@loria.fr>
2021-12-02 12:28:39 +0100

    improved unit tests

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# advent-of-code-2021
+# Advent Of Code 2021
 
 Learning Rust
 
 # Command
 
 `cargo run 1` to run the first puzzle
+
 `cargo test` to run unit tests
--- tests/test_day01.rs
+++ tests/test_day01.rs
@@ -1,10 +1,7 @@
-// use adder;
 use advent_of_code::day01::part1;
 use advent_of_code::day01::part2;
 
-#[test]
-fn test_part1() {
-    let inputs = "199
+const INPUT: &str = "199
 200
 208
 210
@@ -14,24 +11,17 @@ fn test_part1() {
 269
 260
 263";
+
+#[test]
+fn test_part1() {
     let expected = 7;
-    let result = part1(inputs.to_string());
+    let result = part1(INPUT.to_string());
     assert_eq!(expected, result);
 }
 
 #[test]
 fn test_part2() {
-    let inputs = "199
-200
-208
-210
-200
-207
-240
-269
-260
-263";
     let expected = 5;
-    let result = part2(inputs.to_string());
+    let result = part2(INPUT.to_string());
     assert_eq!(expected, result);
 }

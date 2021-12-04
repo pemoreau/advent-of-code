4d06dafbbabc19ecf2c21187f9a68fb5b1be96e9
pem <pierre-etienne.moreau@loria.fr>
2021-12-04 12:16:51 +0100

    day03

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // Days
 pub mod day01;
 pub mod day02;
+pub mod day03;
 
 pub fn noop(_inp: String) -> i32 {
     return 0;
@@ -12,6 +13,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn) {
     match day {
         1 => (day01::part1, day01::part2),
         2 => (day02::part1, day02::part2),
+        3 => (day03::part1, day03::part2),
         _ => {
             println!("Unknown day: {}", day);
             (noop, noop)

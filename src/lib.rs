05ff2f58cdba14e0d4cca0b8c02bc18cadfe1e1a
pem <pierre-etienne.moreau@loria.fr>
2021-12-05 15:13:03 +0100

    day05

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod day01;
 pub mod day02;
 pub mod day03;
 pub mod day04;
+pub mod day05;
 
 pub fn noop(_inp: String) -> i32 {
     return 0;
@@ -16,6 +17,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn) {
         2 => (day02::part1, day02::part2),
         3 => (day03::part1, day03::part2),
         4 => (day04::part1, day04::part2),
+        5 => (day05::part1, day05::part2),
         _ => {
             println!("Unknown day: {}", day);
             (noop, noop)
---
+++ tests/test_day05.rs
@@ -0,0 +1,28 @@
+use advent_of_code::day05::part1;
+use advent_of_code::day05::part2;
+
+const INPUT: &str = "0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2";
+
+#[test]
+
+fn test_part1() {
+    let expected = 5;
+    let result = part1(INPUT.to_string());
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn test_part2() {
+    let expected = 12;
+    let result = part2(INPUT.to_string());
+    assert_eq!(expected, result);
+}

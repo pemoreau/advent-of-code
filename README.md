d1acb98897552fde8975d5c1c0bb6f765dea86ca
pem <pierre-etienne.moreau@loria.fr>
2021-12-06 18:18:44 +0100

    improved parsing speed

--- README.md
+++ README.md
@@ -10,10 +10,12 @@ Learning Rust
 
 # Timings
 
+Execution time on an old Mac Pro (Late 2013), 3,7 GHz Quad-Core Intel Xeon E5
+
 |                         | part A     | part B     |
 | :---------------------- | :--------- | :--------- |
 | [day 1](./src/day01.rs) | ` 0.089ms` | ` 0.067ms` |
-| [day 2](./src/day02.rs) | ` 51.92ms` | ` 45.98ms` |
+| [day 2](./src/day02.rs) | ` 0.092ms` | ` 0.063ms` |
 | [day 3](./src/day03.rs) | ` 0.157ms` | ` 0.084ms` |
 | [day 4](./src/day04.rs) | ` 1.048ms` | ` 0.841ms` |
 | [day 5](./src/day05.rs) | ` 45.94ms` | ` 46.03ms` |
--- src/day02.rs
+++ src/day02.rs
@@ -1,4 +1,4 @@
-fn parse_line1(s: &str) -> (&str, usize) {
+fn _parse_line1(s: &str) -> (&str, usize) {
     let re = regex::Regex::new(r"([a-z]+) ([0-9]+)").unwrap();
     let cap = re.captures(s).unwrap();
     let order: &str = cap.get(1).unwrap().as_str();
@@ -8,10 +8,15 @@ fn parse_line1(s: &str) -> (&str, usize) {
 
 // using for-loop style
 pub fn part1(input: String) -> i64 {
-    let values: Vec<_> = input.lines().map(|line| parse_line1(line)).collect();
+    // let values: Vec<_> = input.lines().map(|line| parse_line1(line)).collect();
+    let values: Vec<_> = input
+        .lines()
+        .map(|line| line.split_once(" ").unwrap())
+        .collect();
     let mut horizontal = 0;
     let mut depth = 0;
-    for (order, value) in values {
+    for (order, param) in values {
+        let value = param.parse::<i32>().unwrap();
         if order == "forward" {
             horizontal += value;
         }
@@ -28,11 +33,15 @@ pub fn part1(input: String) -> i64 {
 
 // using fold()
 pub fn part2(input: String) -> i64 {
-    let values: Vec<_> = input.lines().map(|line| parse_line1(line)).collect();
+    let values: Vec<_> = input
+        .lines()
+        .map(|line| line.split_once(" ").unwrap())
+        .collect();
     let (horizontal, depth, _aim) =
         values
             .iter()
-            .fold((0, 0, 0), |(horizontal, depth, aim), (order, value)| {
+            .fold((0, 0, 0), |(horizontal, depth, aim), (order, param)| {
+                let value = param.parse::<i32>().unwrap();
                 if *order == "forward" {
                     (horizontal + value, depth + (aim * value), aim)
                 } else if *order == "up" {

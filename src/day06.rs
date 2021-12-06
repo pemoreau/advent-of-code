51d401f98328b5ffd994c704e93eb69cb6eeb477
pem <pierre-etienne.moreau@loria.fr>
2021-12-06 14:40:08 +0100

    improved parsing

--- src/day06.rs
+++ src/day06.rs
@@ -8,29 +8,12 @@ pub fn part2(input: String) -> i32 {
 }
 
 pub fn simulate(input: String, n: usize) -> i64 {
-    let values = input
-        .split(',')
-        .map(|s| s.trim().parse::<usize>().unwrap())
-        .collect::<Vec<usize>>();
-
-    let mut mult = values.iter().fold([0; 9], |mut acc, &value| {
-        acc[value] += 1;
+    let mut mult = input.split(',').fold([0; 9], |mut acc, value| {
+        acc[value.trim().parse::<usize>().unwrap()] += 1;
         acc
     });
 
-    for _i in 0..n {
-        // mult = mult
-        //     .iter()
-        //     .enumerate()
-        //     .fold([0; 9], |mut acc, (index, &m)| {
-        //         if index == 0 {
-        //             acc[6] += m;
-        //             acc[8] += m;
-        //         } else {
-        //             acc[index - 1] += m;
-        //         }
-        //         acc
-        //     });
+    for _ in 0..n {
         mult.rotate_left(1);
         mult[6] += mult[8];
     }

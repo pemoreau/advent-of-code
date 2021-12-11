dde7a2f986f859be3eb329d76d0335bde1d1e0e7
pem <pierre-etienne.moreau@loria.fr>
2021-12-11 13:14:19 +0100

    more idiomatic code

--- src/day11.rs
+++ src/day11.rs
@@ -1,18 +1,12 @@
+use itertools::Itertools;
+
 #[derive(Debug)]
 struct Octopus {
     energy: u32,
     flashed: bool,
 }
 
-fn increase_energy(board: &mut Vec<Vec<Octopus>>) {
-    for y in 0..board.len() {
-        for x in 0..board[y].len() {
-            board[y][x].energy += 1;
-        }
-    }
-}
-
-fn increase_neighbors_energy(board: &mut Vec<Vec<Octopus>>, x: usize, y: usize) {
+fn increase_neighbours_energy(board: &mut Vec<Vec<Octopus>>, x: usize, y: usize) {
     for j in (y as i32 - 1)..(y + 1 + 1) as i32 {
         for i in (x as i32 - 1)..(x + 1 + 1) as i32 {
             if j >= 0 && j < board.len() as i32 && i >= 0 && i < board[j as usize].len() as i32 {
@@ -23,24 +17,20 @@ fn increase_neighbors_energy(board: &mut Vec<Vec<Octopus>>, x: usize, y: usize) 
     board[y][x].energy -= 1;
 }
 
-fn clear_flash(board: &mut Vec<Vec<Octopus>>) -> bool {
-    let mut all_falshed = true;
+fn clear_flash(board: &mut Vec<Vec<Octopus>>) {
     for y in 0..board.len() {
         for x in 0..board[y].len() {
             if board[y][x].flashed {
                 board[y][x].energy = 0;
                 board[y][x].flashed = false;
-            } else {
-                all_falshed = false;
             }
         }
     }
-    return all_falshed;
 }
 
-fn flash(board: &mut Vec<Vec<Octopus>>) -> u32 {
-    let mut continue_flashing = true;
+fn flash(board: &mut Vec<Vec<Octopus>>) -> usize {
     let mut flashed = 0;
+    let mut continue_flashing = true;
     while continue_flashing {
         continue_flashing = false;
         for y in 0..board.len() {
@@ -49,16 +39,24 @@ fn flash(board: &mut Vec<Vec<Octopus>>) -> u32 {
                     board[y][x].flashed = true;
                     flashed += 1;
                     continue_flashing = true;
-                    increase_neighbors_energy(board, x, y);
+                    increase_neighbours_energy(board, x, y);
                 }
             }
         }
     }
+    clear_flash(board);
     return flashed;
 }
 
-pub fn part1(input: String) -> i64 {
-    let mut board: Vec<Vec<Octopus>> = input
+fn step(board: &mut Vec<Vec<Octopus>>) -> usize {
+    for (y, x) in (0..board.len()).cartesian_product(0..board[0].len()) {
+        board[x][y].energy += 1;
+    }
+    flash(board)
+}
+
+fn build_board(input: &str) -> Vec<Vec<Octopus>> {
+    input
         .lines()
         .map(|line| {
             line.chars()
@@ -68,40 +66,16 @@ pub fn part1(input: String) -> i64 {
                 })
                 .collect()
         })
-        .collect();
+        .collect()
+}
 
-    let mut res = 0;
-    for _ in 1..101 {
-        increase_energy(&mut board);
-        let flashed = flash(&mut board);
-        res += flashed;
-        clear_flash(&mut board);
-    }
-
-    return res as i64;
+pub fn part1(input: String) -> i64 {
+    let mut board = build_board(&input);
+    (0..100).map(|_| step(&mut board)).sum::<usize>() as i64
 }
 
 pub fn part2(input: String) -> i64 {
-    let mut board: Vec<Vec<Octopus>> = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| Octopus {
-                    energy: c.to_digit(10).unwrap(),
-                    flashed: false,
-                })
-                .collect()
-        })
-        .collect();
-
-    let mut i = 1;
-    loop {
-        increase_energy(&mut board);
-        flash(&mut board);
-        let all_flashed = clear_flash(&mut board);
-        if all_flashed {
-            return i;
-        }
-        i += 1
-    }
+    let mut board = build_board(&input);
+    let n = board.len() * board[0].len();
+    (1..).find(|_| step(&mut board) == n).unwrap() as i64
 }

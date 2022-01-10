9369f6f5d5d353fb67619470a095f640acbe9728
pem <pierre-etienne.moreau@loria.fr>
2022-01-10 14:06:02 +0100

    rm dead code

--- go/23/day23_test.go
+++ go/23/day23_test.go
@@ -1,28 +1,8 @@
 package main
 
 import (
-	_ "embed"
 	"testing"
 )
-
-//go:embed input_test.txt
-var input_test string
-
-// func TestPart1(t *testing.T) {
-// 	result := Part1(string(input_test))
-// 	expected := 0
-// 	if result != expected {
-// 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
-// 	}
-// }
-
-// func TestPart2(t *testing.T) {
-// 	result := Part2(string(input_test))
-// 	expected := 0
-// 	if result != expected {
-// 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
-// 	}
-// }
 
 func TestPart1Input(t *testing.T) {
 	result := Part1(string(input_day))

c99cd70cd41404d84fa975ea2ace032a0b09996a
pem <pierre-etienne.moreau@loria.fr>
2022-04-14 08:33:37 +0200

    start day16 part2

--- rust/2020/day16/src/lib.rs
+++ rust/2020/day16/src/lib.rs
@@ -1,23 +1,5 @@
 use std::collections::HashMap;
 
-pub fn part1(input: String) -> i64 {
-    let e = parse_entries(&input[..]);
-
-    // println!("{:?}", e);
-    let valid = e
-        .nearby
-        .iter()
-        .map(|f| invalid_nearby_ticket(f, &e.constraints))
-        .sum::<u32>();
-    println!("{:?}", valid);
-
-    valid as i64
-}
-
-pub fn part2(input: String) -> i64 {
-    0
-}
-
 #[derive(Debug)]
 enum Constraint {
     Field(String, Interval, Interval),
@@ -28,6 +10,13 @@ enum Interval {
     Range(u32, u32),
 }
 
+#[derive(Debug)]
+struct Data {
+    ticket: Vec<u32>,
+    nearby: Vec<Vec<u32>>,
+    constraints: Vec<Constraint>,
+}
+
 impl Interval {
     fn contains(&self, val: u32) -> bool {
         match self {
@@ -36,47 +25,86 @@ impl Interval {
     }
 }
 
-#[derive(Debug)]
-struct Data {
-    ticket: Vec<u32>,
-    nearby: Vec<Vec<u32>>,
-    constraints: Vec<Constraint>,
+pub fn part1(input: String) -> i64 {
+    let e = parse_entries(&input[..]);
+    let valid = e
+        .nearby
+        .iter()
+        .map(|f| invalid_nearby_ticket(f, &e.constraints))
+        .sum::<u32>();
+    valid as i64
 }
 
-fn invalid_field(f: u32, constraints: &Vec<Constraint>) -> Option<u32> {
-    let mut res = None;
-    for c in constraints {
-        match c {
-            Constraint::Field(_, i, j) => {
-                if !i.contains(f) && !j.contains(f) {
-                    println!("{} {:?} {:?}", f, i, j);
-                    res = Some(f);
-                } else {
-                    println!("ok {} {:?} {:?}", f, i, j);
-                    return None;
+pub fn part2(input: String) -> i64 {
+    let e = parse_entries(&input[..]);
+
+    let valid_nearby = e
+        .nearby
+        .iter()
+        .filter(|&f| invalid_nearby_ticket(f, &e.constraints) == 0)
+        .collect::<Vec<_>>();
+
+    // possible index for each field
+    let mut map: HashMap<String, Vec<bool>> = HashMap::new();
+    for c in e.constraints.iter() {
+        let Constraint::Field(name, _, _) = c;
+        let mut v = Vec::<bool>::new();
+        (0..e.ticket.len()).for_each(|_| v.push(true));
+        map.insert(name.clone(), v);
+    }
+
+    println!("map={:?}", map);
+    // eliminate index which are not satisfying constraints
+    for c in e.constraints.iter() {
+        let Constraint::Field(name, i1, i2) = c;
+        for valid in valid_nearby.iter() {
+            for (i, n) in valid.iter().enumerate() {
+                if !i1.contains(*n) && !i2.contains(*n) {
+                    println!("c={:?} n={}, i={} i1={:?} i2={:?}", c, n, i, i1, i2);
+                    let v = map.get_mut(name).unwrap();
+                    v[i] = false;
+                    println!("v={:?}", v);
                 }
+            }
+        }
+    }
+
+    // find constraint with only one true and remove index from others
+    for flags in map.values() {
+        println!("flags={:?} index={:?}", flags, single_index(flags));
+    }
+    println!("map={:?}", map);
+    0
+}
+
+fn single_index(v: &Vec<bool>) -> Option<usize> {
+    let mut res = None;
+    for (i, &b) in v.iter().enumerate() {
+        if b {
+            if res.is_none() {
+                res = Some(i);
+            } else {
+                return None;
             }
         }
     }
     res
 }
 
-fn invalid_nearby_ticket(t: &Vec<u32>, constraints: &Vec<Constraint>) -> u32 {
-    let res = t
-        .iter()
-        .filter(|f| invalid_field(**f, constraints).is_some())
-        .sum();
-    if res > 0 {
-        println!("{:?} {}", t, res);
+fn invalid_field(f: u32, constraints: &Vec<Constraint>) -> Option<u32> {
+    for c in constraints {
+        let Constraint::Field(_, i, j) = c;
+        if i.contains(f) || j.contains(f) {
+            return None;
+        }
     }
-    res
+    Some(f)
+}
 
-    // for n in t.iter() {
-    //     if !valid_field(*n, &constraints) {
-    //         return false;
-    //     }
-    // }
-    // true
+fn invalid_nearby_ticket(t: &Vec<u32>, constraints: &Vec<Constraint>) -> u32 {
+    t.iter()
+        .filter(|f| invalid_field(**f, constraints).is_some())
+        .sum()
 }
 
 fn parse_entries(s: &str) -> Data {

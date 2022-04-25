332b1ef0f01ca7e4139828341984497e33d1f6e7
pem <pierre-etienne.moreau@loria.fr>
2022-04-25 11:27:05 +0200

    use cartesian products but it is slower

--- rust/2020/day17/src/lib.rs
+++ rust/2020/day17/src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
 
+use itertools::Itertools;
+
 pub fn part1(input: String) -> i64 {
     solve(input, false)
 }
@@ -26,45 +28,67 @@ fn neiboors(
     (x, y, z, w): (i32, i32, i32, i32),
     d4: bool,
 ) -> usize {
-    let mut active = 0;
-    let domain4 = if d4 { -1..2 } else { 0..1 };
-    for i in -1..2 {
-        for j in -1..2 {
-            for k in -1..2 {
-                for l in domain4.clone() {
-                    if i == 0 && j == 0 && k == 0 && l == 0 {
-                        continue;
-                    }
-                    if state.contains(&(x + i, y + j, z + k, w + l)) {
-                        active += 1;
-                    }
-                }
-            }
-        }
-    }
-    active
+    // let mut active = 0;
+    // let domain4 = if d4 { -1..2 } else { 0..1 };
+    // for i in -1..2 {
+    //     for j in -1..2 {
+    //         for k in -1..2 {
+    //             for l in domain4.clone() {
+    //                 if i == 0 && j == 0 && k == 0 && l == 0 {
+    //                     continue;
+    //                 }
+    //                 if state.contains(&(x + i, y + j, z + k, w + l)) {
+    //                     active += 1;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // active
+
+    // shorter but slower solution
+    (0..4)
+        .map(|i| if i == 3 && !d4 { 0..1 } else { -1..2 })
+        .multi_cartesian_product()
+        .filter(|v| {
+            !(v[0] == 0 && v[1] == 0 && v[2] == 0 && v[3] == 0)
+                && state.contains(&(x + v[0], y + v[1], z + v[2], w + v[3]))
+        })
+        .count()
 }
 
 fn step(state: &HashSet<(i32, i32, i32, i32)>, d4: bool) -> HashSet<(i32, i32, i32, i32)> {
     let mut new_state: HashSet<(i32, i32, i32, i32)> = HashSet::new();
-    let domain4 = if d4 { -1..2 } else { 0..1 };
+    // let domain4 = if d4 { -1..2 } else { 0..1 };
+    // state.iter().for_each(|(x, y, z, w)| {
+    //     for i in -1..2 {
+    //         for j in -1..2 {
+    //             for k in -1..2 {
+    //                 for l in domain4.clone() {
+    //                     let cell = (x + i, y + j, z + k, w + l);
+    //                     let active = neiboors(state, cell, d4);
+    //                     if active == 3 || (state.contains(&cell) && active == 2) {
+    //                         new_state.insert(cell);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // });
+
+    // shorter but slower solution
     state.iter().for_each(|(x, y, z, w)| {
-        for i in -1..2 {
-            for j in -1..2 {
-                for k in -1..2 {
-                    for l in domain4.clone() {
-                        let cell = (x + i, y + j, z + k, w + l);
-                        let active = neiboors(state, cell, d4);
-                        if state.contains(&cell) && (active == 2 || active == 3) {
-                            new_state.insert(cell);
-                        }
-                        if !state.contains(&cell) && active == 3 {
-                            new_state.insert(cell);
-                        }
-                    }
+        (0..4)
+            .map(|i| if i == 3 && !d4 { 0..1 } else { -1..2 })
+            .multi_cartesian_product()
+            .for_each(|v| {
+                let cell = (x + v[0], y + v[1], z + v[2], w + v[3]);
+                let active = neiboors(state, cell, d4);
+                if active == 3 || (state.contains(&cell) && active == 2) {
+                    new_state.insert(cell);
                 }
-            }
-        }
+            })
     });
+
     new_state
 }

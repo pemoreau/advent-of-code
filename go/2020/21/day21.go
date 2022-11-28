49556a325340d9b85d03f60fdca3f8c4ce4ee746
pem <pierre-etienne.moreau@loria.fr>
2022-11-28 08:08:54 +0100

    parsing

--- go/2020/21/day21.go
+++ go/2020/21/day21.go
@@ -3,15 +3,71 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strings"
 	"time"
 )
 
-//go:embed input.txt
+//go:embed input_test.txt
 var input_day string
 
+type Allergen string
+type Ingredient string
+
+func fillIndex(list []string, index int, mapIndex map[string]int) int {
+	for _, a := range list {
+		if _, ok := mapIndex[a]; !ok {
+			mapIndex[a] = index
+			index++
+		}
+	}
+	return index
+}
+
 func Part1(input string) int {
-	// input = strings.TrimSuffix(input, "\n")
-	// lines := strings.Split(input, "\n")
+	input = strings.TrimSuffix(input, "\n")
+	lines := strings.Split(input, "\n")
+	allergenIndex := make(map[string]int)
+	ingredientIndex := make(map[string]int)
+	aIndex := 0
+	iIndex := 0
+
+	ingredients := make([][]string, 0, len(lines))
+	allergens := make([][]string, 0, len(lines))
+	for _, line := range lines {
+		parts := strings.Split(line, " (contains ")
+		iList := strings.Split(parts[0], " ")
+		parts[1] = strings.TrimSuffix(parts[1], ")")
+		aList := strings.Split(parts[1], ", ")
+
+		ingredients = append(ingredients, iList)
+		allergens = append(allergens, aList)
+
+		iIndex = fillIndex(iList, iIndex, ingredientIndex)
+		aIndex = fillIndex(aList, aIndex, allergenIndex)
+	}
+	//fmt.Println("ingredients", ingredients)
+	//fmt.Println("allergens", allergens)
+
+	iConstraint := make([][]bool, len(lines))
+	aConstraint := make([][]bool, len(lines))
+	for line := 0; line < len(lines); line++ {
+		iConstraint[line] = make([]bool, iIndex)
+		aConstraint[line] = make([]bool, aIndex)
+		for _, i := range ingredients[line] {
+			iConstraint[line][ingredientIndex[i]] = true
+		}
+		for _, a := range allergens[line] {
+			aConstraint[line][allergenIndex[a]] = true
+		}
+	}
+
+	fmt.Println(ingredientIndex)
+	fmt.Println(allergenIndex)
+
+	for i := 0; i < len(lines); i++ {
+		fmt.Println(iConstraint[i], aConstraint[i])
+	}
+
 	return 0
 }
 

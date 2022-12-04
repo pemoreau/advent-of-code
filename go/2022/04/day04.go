da96aff752d520693f5df9aa755d6dc25c5960eb
pem <pierre-etienne.moreau@loria.fr>
2022-12-04 14:46:29 +0100

    use scanner to improve efficiency

--- go/2022/04/day04.go
+++ go/2022/04/day04.go
@@ -3,7 +3,9 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
 	"strings"
+	"text/scanner"
 	"time"
 )
 
@@ -11,6 +13,57 @@ import (
 var input_day string
 
 func Part1(input string) int {
+	//input = strings.TrimSuffix(input, "\n")
+	var s scanner.Scanner
+	s.Init(strings.NewReader(input))
+	s.Filename = "example"
+	s.Mode ^= scanner.SkipComments  // don't skip comments
+	s.Whitespace |= 1<<'-' | 1<<',' // skip minus and comma
+
+	res := 0
+	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
+		//fmt.Printf("%s: %s\n", s.Position, s.TokenText())
+		a, _ := strconv.Atoi(s.TokenText())
+		//s.Scan() // skip -
+		s.Scan()
+		b, _ := strconv.Atoi(s.TokenText())
+		//s.Scan() // skip ,
+		s.Scan()
+		c, _ := strconv.Atoi(s.TokenText())
+		//s.Scan() // skip -
+		s.Scan()
+		d, _ := strconv.Atoi(s.TokenText())
+		// check inclusion
+		if (a <= c && d <= b) || (c <= a && b <= d) {
+			res++
+		}
+	}
+	return res
+}
+
+func Part2(input string) int {
+	//input = strings.TrimSuffix(input, "\n")
+	var s scanner.Scanner
+	s.Init(strings.NewReader(input))
+	s.Whitespace |= 1<<'-' | 1<<',' // skip minus and comma
+	res := 0
+	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
+		a, _ := strconv.Atoi(s.TokenText())
+		s.Scan()
+		b, _ := strconv.Atoi(s.TokenText())
+		s.Scan()
+		c, _ := strconv.Atoi(s.TokenText())
+		s.Scan()
+		d, _ := strconv.Atoi(s.TokenText())
+		// check overlap
+		if !(b < c || d < a) {
+			res++
+		}
+	}
+	return res
+}
+
+func Part1Slow(input string) int {
 	input = strings.TrimSuffix(input, "\n")
 	lines := strings.Split(input, "\n")
 	res := 0
@@ -25,7 +78,7 @@ func Part1(input string) int {
 	return res
 }
 
-func Part2(input string) int {
+func Part2Slow(input string) int {
 	input = strings.TrimSuffix(input, "\n")
 	lines := strings.Split(input, "\n")
 	res := 0
--- go/2022/04/day04_test.go
+++ go/2022/04/day04_test.go
@@ -24,21 +24,21 @@ func TestPart1Input(t *testing.T) {
 	}
 }
 
-//func TestPart2(t *testing.T) {
-//	result := Part2(string(input_test))
-//	expected := 0
-//	if result != expected {
-//		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
-//	}
-//}
+func TestPart2(t *testing.T) {
+	result := Part2(string(input_test))
+	expected := 4
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
 
-//func TestPart2Input(t *testing.T) {
-//	result := Part2(string(input_day))
-//	expected := 0
-//	if result != expected {
-//		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
-//	}
-//}
+func TestPart2Input(t *testing.T) {
+	result := Part2(string(input_day))
+	expected := 914
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
 
 func BenchmarkPart1(b *testing.B) {
 	for i := 0; i < b.N; i++ {

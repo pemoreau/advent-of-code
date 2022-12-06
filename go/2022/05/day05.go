aac3a3c78bac4e814efb68c55f061cd42e25e56c
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2022-12-06 15:13:22 +0100

    use tab[26]

--- go/2022/05/day05.go
+++ go/2022/05/day05.go
@@ -34,7 +34,7 @@ func BuildStacks(part0 string) []utils.Stack[uint8] {
 	return stacks
 }
 
-func reverse[S ~[]E, E any](s S) {
+func reverse[T any](s []T) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}

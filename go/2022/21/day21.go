192ff53d86820e346bf60b8fb061b70592afc3de
pem <pierre-etienne.moreau@loria.fr>
2022-12-21 15:59:50 +0100

    Day 21 part 2

--- go/2022/21/day21.go
+++ go/2022/21/day21.go
@@ -12,33 +12,54 @@ import (
 var input_day string
 
 type Expr interface {
-	isExpr()
-	eval(mem map[string]Expr) int
+	//eval(mem map[string]Expr, cache map[string]Value) Value
 	simplify(mem map[string]Expr) Expr
-	solve(mem map[string]Expr) Expr
+	isValue() bool
 }
 
-type Value int
+type Value struct {
+	n int
+	d int
+}
 type Var string
 type Op struct {
-	op  byte
+	Op  byte
 	lhs Expr
 	rhs Expr
 }
 
-func (e Value) isExpr() {}
-func (e Op) isExpr()    {}
-func (e Var) isExpr()   {}
+func (e Value) isValue() bool { return true }
+func (e Op) isValue() bool    { return false }
+func (e Var) isValue() bool   { return false }
 
 func (e Value) String() string {
-	return strconv.Itoa(int(e))
+	return fmt.Sprintf("%d/%d", e.n, e.d)
 }
+
 func (e Var) String() string {
 	return string(e)
 }
 
 func (e Op) String() string {
-	return fmt.Sprintf("(%v %c %v)", e.lhs, e.op, e.rhs)
+	return fmt.Sprintf("(%v %c %v)", e.lhs, e.Op, e.rhs)
+}
+
+func isInt(e Expr) bool {
+	if e.isValue() {
+		n := e.(Value).n
+		d := e.(Value).d
+		return n%d == 0
+	}
+	return false
+}
+
+func getInt(e Expr) int {
+	if e.isValue() {
+		n := e.(Value).n
+		d := e.(Value).d
+		return n / d
+	}
+	panic("not int")
 }
 
 func (e Var) display(mem map[string]Expr) string {
@@ -47,7 +68,12 @@ func (e Var) display(mem map[string]Expr) string {
 	case Var:
 		return "var " + string(v.(Var))
 	case Value:
-		return strconv.Itoa(int(v.(Value)))
+		v := v.(Value)
+		if v.d == 1 {
+			return fmt.Sprintf("%d", v.n)
+		} else {
+			return fmt.Sprintf("%d/%d", v.n, v.d)
+		}
 	case Op:
 		return fmt.Sprintf("(%v)", v.(Op).display(mem))
 	}
@@ -57,7 +83,7 @@ func (e Var) display(mem map[string]Expr) string {
 func (e Op) display(mem map[string]Expr) string {
 	lhs := e.lhs.(Var).display(mem)
 	rhs := e.rhs.(Var).display(mem)
-	return fmt.Sprintf("%v %c %v", lhs, e.op, rhs)
+	return fmt.Sprintf("%v %c %v", lhs, e.Op, rhs)
 }
 
 func (e Value) simplify(mem map[string]Expr) Expr {
@@ -69,159 +95,195 @@ func (e Var) simplify(mem map[string]Expr) Expr {
 	if ok {
 		return v.simplify(mem)
 	}
-	fmt.Println("simplify var ", e, " := ", v)
+	//fmt.Println("simplify var ", e, " do noting ")
 	return e
 }
 
+func GCDRemainderRecursive(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return GCDRemainderRecursive(b, a%b)
+}
 func (e Op) simplify(mem map[string]Expr) Expr {
 	lhs := e.lhs.simplify(mem)
 	rhs := e.rhs.simplify(mem)
 	switch lhs.(type) {
 	case Value:
+		lv := lhs.(Value)
 		switch rhs.(type) {
 		case Value:
-			return Value(e.eval(mem))
+			rv := rhs.(Value)
+			var tmp Value
+			switch e.Op {
+			case '+':
+				tmp = Value{lv.n*rv.d + rv.n*lv.d, lv.d * rv.d}
+			case '*':
+				tmp = Value{lv.n * rv.n, lv.d * rv.d}
+			case '/':
+				tmp = Value{lv.n * rv.d, lv.d * rv.n}
+			case '-':
+				tmp = Value{lv.n*rv.d - rv.n*lv.d, lv.d * rv.d}
+			default:
+				fmt.Printf("unknown op %c\n", e.Op)
+				panic("unknown op")
+			}
+			if isInt(tmp) {
+				return Value{getInt(tmp), 1}
+			}
+			gcd := GCDRemainderRecursive(tmp.n, tmp.d)
+			tmp.n /= gcd
+			tmp.d /= gcd
+			//fmt.Println("simplify ", e, " to ", tmp)
+			return tmp
+
 		}
 	}
-	return Op{e.op, lhs, rhs}
+	return Op{Op: e.Op, lhs: lhs, rhs: rhs}
 }
 
-func (e Value) eval(mem map[string]Expr) int {
-	//fmt.Println("eval", e, " --> ", int(e))
-	return int(e)
-}
-func (e Var) eval(mem map[string]Expr) int {
-	if v, ok := cache[string(e)]; ok {
-		//fmt.Println("cache", e, " --> ", v)
-		return v
+func (e Op) solve(v Var, mem map[string]Expr) Expr {
+	//fmt.Println("solve e = ", e)
+	lhs := e.lhs.simplify(mem)
+	rhs := e.rhs.simplify(mem)
+	fmt.Println("solve", v, " in ", lhs, " = ", rhs)
+	if lhs.isValue() {
+		lhs, rhs = rhs, lhs
 	}
-	v := mem[string(e)].eval(mem)
-	//fmt.Println("eval", e, " --> ", v)
-	cache[string(e)] = v
-	return v
-}
-
-func (e Op) eval(mem map[string]Expr) int {
-	lhs := e.lhs.eval(mem)
-	rhs := e.rhs.eval(mem)
-	switch e.op {
-	case '+':
-		return lhs + rhs
-	case '*':
-		return lhs * rhs
-	case '/':
-		return lhs / rhs
-	case '-':
-		return lhs - rhs
-	case '=':
-		//fmt.Println("lhs", lhs)
-		//fmt.Println("rhs", rhs)
-		return lhs - rhs
+	// lhs is an expr, not a value
+	if lhs == v {
+		return rhs
 	}
-	panic("unknown op")
+	if !rhs.isValue() {
+		panic("rhs is not a value")
+	}
+
+	switch lhs.(type) {
+	case Value:
+		if lhs.(Value) == rhs.(Value) {
+			return e
+		}
+		panic("lhs is value")
+	case Var:
+		if lhs.(Var) == v {
+			return e
+		}
+		panic("lhs is not the correct var")
+	case Op:
+		// lhs is: a Op b
+		o := lhs.(Op)
+		a := o.lhs.simplify(mem)
+		b := o.rhs.simplify(mem)
+		//fmt.Printf("\ndo %c %v\n\n", inverse[o.Op], b.(Value))
+		var newEq Op
+		if o.Op == '+' {
+			if !a.isValue() {
+				newEq = Op{Op: '=', lhs: a, rhs: Op{Op: '-', lhs: rhs, rhs: b}}
+			}
+			if !b.isValue() {
+				newEq = Op{Op: '=', lhs: b, rhs: Op{Op: '-', lhs: rhs, rhs: a}}
+			}
+		}
+		if o.Op == '*' {
+			if !a.isValue() {
+				newEq = Op{Op: '=', lhs: a, rhs: Op{Op: '/', lhs: rhs, rhs: b}}
+			}
+			if !b.isValue() {
+				newEq = Op{Op: '=', lhs: b, rhs: Op{Op: '/', lhs: rhs, rhs: a}}
+			}
+		}
+		if o.Op == '-' {
+			if !a.isValue() {
+				newEq = Op{Op: '=', lhs: a, rhs: Op{Op: '+', lhs: rhs, rhs: b}}
+			}
+			if !b.isValue() {
+				newEq = Op{Op: '=', lhs: b, rhs: Op{Op: '-', lhs: a, rhs: rhs}}
+			}
+		}
+		if o.Op == '/' {
+			if !a.isValue() {
+				newEq = Op{Op: '=', lhs: a, rhs: Op{Op: '*', lhs: rhs, rhs: b}}
+			}
+			if !b.isValue() {
+				newEq = Op{Op: '=', lhs: b, rhs: Op{Op: '/', lhs: a, rhs: rhs}}
+			}
+		}
+
+		return newEq.solve(v, mem)
+	}
+	panic("unknown solve")
 }
 
-var cache map[string]int
-
-func Part1(input string) int {
+func parse(input string) map[string]Expr {
 	input = strings.TrimSuffix(input, "\n")
 	lines := strings.Split(input, "\n")
 
 	mem := make(map[string]Expr)
-	cache = make(map[string]int)
+	//cache = make(map[string]Value)
 	for _, line := range lines {
 		values := strings.Split(line, " ")
 		vname := strings.TrimSuffix(values[0], ":")
 		//fmt.Println(len(values), values)
 		if len(values) == 2 {
 			e, _ := strconv.Atoi(values[1])
-			mem[vname] = Value(e)
+			mem[vname] = Value{e, 1}
 		} else if len(values) == 4 {
 			e := Op{
-				op:  values[2][0],
+				Op:  values[2][0],
 				lhs: Var(values[1]),
 				rhs: Var(values[3]),
 			}
 			mem[vname] = e
 		}
 	}
+	return mem
+}
+
+func Part1(input string) int {
+	mem := parse(input)
 
 	//for e := range mem {
 	//	fmt.Println(e, " := ", mem[e])
 	//}
-	res := Var("root").eval(mem)
+	res := 0
+	//res := Var("root").eval(mem)
+
+	rootExpr := mem["root"]
+	delete(mem, "root")
+	e := Op{
+		Op:  '=',
+		lhs: Var("root"),
+		rhs: rootExpr,
+	}.solve("root", mem)
+	fmt.Println("solve", e)
+
 	return res
 }
 
 func Part2(input string) int {
-	input = strings.TrimSuffix(input, "\n")
-	lines := strings.Split(input, "\n")
-
-	mem := make(map[string]Expr)
-	for _, line := range lines {
-		values := strings.Split(line, " ")
-		vname := strings.TrimSuffix(values[0], ":")
-		//fmt.Println(len(values), values)
-		if len(values) == 2 {
-			e, _ := strconv.Atoi(values[1])
-			mem[vname] = Value(e)
-		} else if len(values) == 4 {
-			e := Op{
-				op:  values[2][0],
-				lhs: Var(values[1]),
-				rhs: Var(values[3]),
-			}
-			mem[vname] = e
-		}
-	}
-	mem["root"] = Op{
-		op:  '=',
-		lhs: mem["root"].(Op).lhs,
-		rhs: mem["root"].(Op).rhs,
-	}
-	//for e := range mem {
-	//	fmt.Println(e, " := ", mem[e])
-	//}
-
-	//mem["humn"] = Value(1234)
-	//fmt.Println("humn", Var("humn").display(mem))
-	cache = make(map[string]int)
-
+	mem := parse(input)
 	delete(mem, "humn")
-	name1 := "mcnw"
-	name2 := "wqdw"
-	e1 := Var(name1).simplify(mem)
-	e2 := Var(name2).simplify(mem)
-	fmt.Println("mcnw", e1.simplify(mem))
-	fmt.Println("wqdw", e2.simplify(mem))
-	fmt.Println(name1, e1)
-	fmt.Println(name2, e2)
+	root := mem["root"].(Op)
+	root.Op = '='
 
-	//switch expr.(type) {
-	//case Op:
-	//	fmt.Println(name, expr.(Op).display(mem))
-	//}
+	eq := root.solve("humn", mem)
+	fmt.Println("humn", eq)
 
-	//a := 1000000000000
-	//for i := a + 0; i < a+30; i = i + 3 {
-	//	//if i%10000 == 0 {
-	//	//	fmt.Println(i)
-	//	//}
-	//	cache = make(map[string]int)
-	//	mem["humn"] = Value(i)
-	//	res := Var("root").eval(mem)
-	//	fmt.Println("humn", i, " --> ", res, res-31343426392931)
-	//	if res == 0 {
-	//		return i
-	//	}
-	//}
+	root.Op = '-'
+	fmt.Println("root", root.simplify(mem))
+	switch eq := eq.(type) {
+	case Value:
+		v := eq.n / eq.d
+		fmt.Println("v", v)
+		return v
+	}
 	return 0
 }
 
 func main() {
 	fmt.Println("--2022 day 21 solution--")
 	start := time.Now()
-	//fmt.Println("part1: ", Part1(input_day))
+	fmt.Println("part1: ", Part1(input_day))
 	fmt.Println(time.Since(start))
 
 	start = time.Now()

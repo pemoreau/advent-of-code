019088eb4c8485b533eb79bbb5ff1cfcdd463243
pem <pierre-etienne.moreau@loria.fr>
2022-12-22 20:48:19 +0100

    Day 22 part 2

--- go/2022/22/day22.go
+++ go/2022/22/day22.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-//go:embed input_test.txt
+//go:embed input.txt
 var input_day string
 
 type Pos struct {
@@ -83,7 +83,7 @@ func parse(input string) (topology *Topology, path string) {
 }
 
 func step(state *State, order string) {
-	fmt.Println("order", order)
+	//fmt.Println("order", order)
 	switch order {
 	case "L":
 		state.Dir = (state.Dir + 3) % 4
@@ -134,17 +134,12 @@ func step(state *State, order string) {
 func Part1(input string) int {
 	topology, path := parse(input)
 	state := State{Pos{topology.row[0].Min, 0}, 0, topology}
-	fmt.Println(path)
-	fmt.Println("row", topology.row)
-	fmt.Println("column", topology.column)
-	fmt.Println("state", state)
-
 	path = strings.ReplaceAll(path, "L", " L ")
 	path = strings.ReplaceAll(path, "R", " R ")
 	orders := strings.Split(path, " ")
 	for _, order := range orders {
 		step(&state, order)
-		fmt.Printf("state: %v\n", state)
+		//fmt.Printf("state: %v\n", state)
 	}
 
 	res := 1000*(state.Pos.Y+1) + 4*(state.Pos.X+1) + state.Dir
@@ -168,22 +163,9 @@ func (s State3D) String() string {
 	return fmt.Sprintf("face %d -- %d,%d  %s", s.face+1, s.pos.Y+1, s.pos.X+1, dir[s.dir])
 }
 
-func (s *State3D) rotateRight() {
+func (s *State3D) rotate90() {
 	s.dir = (s.dir + 1) % 4
-	s.pos.X = s.N - s.pos.Y - 1
-	s.pos.Y = s.pos.X
-}
-
-func (s *State3D) rotateLeft() {
-	s.dir = (s.dir + 3) % 4
-	s.pos.X = s.pos.Y
-	s.pos.Y = s.N - s.pos.X - 1
-}
-
-func (s *State3D) rotate180() {
-	s.dir = (s.dir + 2) % 4
-	s.pos.X = s.N - s.pos.X - 1
-	s.pos.Y = s.N - s.pos.Y - 1
+	s.pos.X, s.pos.Y = s.N-s.pos.Y-1, s.pos.X
 }
 
 func (s *State3D) move(cube *Cube, transitionTable [6][4]struct{ face, rot int }) bool {
@@ -218,18 +200,17 @@ func (s *State3D) move(cube *Cube, transitionTable [6][4]struct{ face, rot int }
 	if s.pos.Y >= s.N {
 		s.pos.Y = 0
 	}
-	//fmt.Println("transition", s.face, s.dir, transitionTable[s.face][s.dir])
 	s.face = transitionTable[start.face][start.dir].face
 	switch transitionTable[start.face][start.dir].rot {
 	case 90:
-		fmt.Println("rotate 90")
-		s.rotateLeft()
+		s.rotate90()
 	case 180:
-		fmt.Println("rotate 180")
-		s.rotate180()
+		s.rotate90()
+		s.rotate90()
 	case 270:
-		fmt.Println("rotate 270")
-		s.rotateRight()
+		s.rotate90()
+		s.rotate90()
+		s.rotate90()
 	}
 	if cube.grid[s.face][s.pos] == '#' {
 		*s = start
@@ -243,7 +224,6 @@ func (s *State3D) move(cube *Cube, transitionTable [6][4]struct{ face, rot int }
 type TransitionTable [6][4]struct{ face, rot int }
 
 func step3D(s *State3D, cube *Cube, order string, transitionTable TransitionTable) {
-	fmt.Println("order", order)
 	switch order {
 	case "L":
 		s.dir = (s.dir + 3) % 4
@@ -252,12 +232,10 @@ func step3D(s *State3D, cube *Cube, order string, transitionTable TransitionTabl
 	default:
 		nbSteps, _ := strconv.Atoi(order)
 		for i := 0; i < nbSteps; i++ {
-			before := *s
 			ok := s.move(cube, transitionTable)
 			if !ok {
 				break
 			}
-			fmt.Println("step", i+1, "move", before, "to", *s)
 		}
 
 	}
@@ -269,26 +247,53 @@ func Part2(input string) int {
 	lines := strings.Split(parts[0], "\n")
 	path := parts[1]
 
-	N := 4
+	//N := 4
+	//faces := [6]struct {
+	//	X utils.Interval
+	//	Y utils.Interval
+	//}{
+	//	{X: utils.Interval{8, 11}, Y: utils.Interval{0, 3}},
+	//	{X: utils.Interval{0, 3}, Y: utils.Interval{4, 7}},
+	//	{X: utils.Interval{4, 7}, Y: utils.Interval{4, 7}},
+	//	{X: utils.Interval{8, 11}, Y: utils.Interval{4, 7}},
+	//	{X: utils.Interval{8, 11}, Y: utils.Interval{8, 11}},
+	//	{X: utils.Interval{12, 15}, Y: utils.Interval{8, 11}},
+	//}
+	//
+	//transition := TransitionTable{
+	//	/* face 1 */ {{face: 6 - 1, rot: 180}, {face: 4 - 1, rot: 0}, {face: 3 - 1, rot: 270}, {face: 2 - 1, rot: 180}},
+	//	/* face 2 */ {{face: 3 - 1, rot: 0}, {face: 5 - 1, rot: 180}, {face: 6 - 1, rot: 90}, {face: 1 - 1, rot: 180}},
+	//	/* face 3 */ {{face: 4 - 1, rot: 0}, {face: 5 - 1, rot: 270}, {face: 2 - 1, rot: 0}, {face: 1 - 1, rot: 90}},
+	//	/* face 4 */ {{face: 6 - 1, rot: 90}, {face: 5 - 1, rot: 0}, {face: 3 - 1, rot: 0}, {face: 1 - 1, rot: 0}},
+	//	/* face 5 */ {{face: 6 - 1, rot: 0}, {face: 2 - 1, rot: 180}, {face: 3 - 1, rot: 90}, {face: 4 - 1, rot: 0}},
+	//	/* face 6 */ {{face: 1 - 1, rot: 180}, {face: 2 - 1, rot: 270}, {face: 5 - 1, rot: 0}, {face: 4 - 1, rot: 270}},
+	//}
+
+	//  12
+	//  3
+	// 45
+	// 6
+
+	N := 50
 	faces := [6]struct {
 		X utils.Interval
 		Y utils.Interval
 	}{
-		{X: utils.Interval{8, 11}, Y: utils.Interval{0, 3}},
-		{X: utils.Interval{0, 3}, Y: utils.Interval{4, 7}},
-		{X: utils.Interval{4, 7}, Y: utils.Interval{4, 7}},
-		{X: utils.Interval{8, 11}, Y: utils.Interval{4, 7}},
-		{X: utils.Interval{8, 11}, Y: utils.Interval{8, 11}},
-		{X: utils.Interval{12, 15}, Y: utils.Interval{8, 11}},
+		{X: utils.Interval{1 * N, 2*N - 1}, Y: utils.Interval{0 * N, 1*N - 1}},
+		{X: utils.Interval{2 * N, 3*N - 1}, Y: utils.Interval{0 * N, 1*N - 1}},
+		{X: utils.Interval{1 * N, 2*N - 1}, Y: utils.Interval{1 * N, 2*N - 1}},
+		{X: utils.Interval{0 * N, 1*N - 1}, Y: utils.Interval{2 * N, 3*N - 1}},
+		{X: utils.Interval{1 * N, 2*N - 1}, Y: utils.Interval{2 * N, 3*N - 1}},
+		{X: utils.Interval{0 * N, 1*N - 1}, Y: utils.Interval{3 * N, 4*N - 1}},
 	}
 
 	transition := TransitionTable{
-		{{face: 6 - 1, rot: 180}, {face: 4 - 1, rot: 0}, {face: 3 - 1, rot: 90}, {face: 2 - 1, rot: 180}},
-		{{face: 3 - 1, rot: 0}, {face: 5 - 1, rot: 180}, {face: 6 - 1, rot: 270}, {face: 1 - 1, rot: 180}},
-		{{face: 4 - 1, rot: 0}, {face: 5 - 1, rot: 90}, {face: 2 - 1, rot: 0}, {face: 1 - 1, rot: 270}},
-		{{face: 6 - 1, rot: 270}, {face: 5 - 1, rot: 0}, {face: 3 - 1, rot: 0}, {face: 1 - 1, rot: 0}},
-		{{face: 6 - 1, rot: 0}, {face: 2 - 1, rot: 180}, {face: 3 - 1, rot: 270}, {face: 4 - 1, rot: 0}},
-		{{face: 1 - 1, rot: 180}, {face: 2 - 1, rot: 90}, {face: 5 - 1, rot: 0}, {face: 4 - 1, rot: 90}},
+		/* face 1 */ {{face: 2 - 1, rot: 0}, {face: 3 - 1, rot: 0}, {face: 4 - 1, rot: 180}, {face: 6 - 1, rot: 90}},
+		/* face 2 */ {{face: 5 - 1, rot: 180}, {face: 3 - 1, rot: 90}, {face: 1 - 1, rot: 0}, {face: 6 - 1, rot: 0}},
+		/* face 3 */ {{face: 2 - 1, rot: 270}, {face: 5 - 1, rot: 0}, {face: 4 - 1, rot: 270}, {face: 1 - 1, rot: 0}},
+		/* face 4 */ {{face: 5 - 1, rot: 0}, {face: 6 - 1, rot: 0}, {face: 1 - 1, rot: 180}, {face: 3 - 1, rot: 90}},
+		/* face 5 */ {{face: 2 - 1, rot: 180}, {face: 6 - 1, rot: 90}, {face: 4 - 1, rot: 0}, {face: 3 - 1, rot: 0}},
+		/* face 6 */ {{face: 5 - 1, rot: 270}, {face: 2 - 1, rot: 0}, {face: 1 - 1, rot: 270}, {face: 4 - 1, rot: 0}},
 	}
 
 	cube := Cube{N: N}
@@ -309,21 +314,18 @@ func Part2(input string) int {
 	orders := strings.Split(path, " ")
 
 	state := State3D{N: N, face: 0, pos: Pos{0, 0}, dir: 0}
-	fmt.Printf("start from state: %v\n", state)
 	for _, order := range orders {
 		step3D(&state, &cube, order, transition)
-		fmt.Printf("state: %v\n", state)
 	}
-	res := 1000*(state.pos.Y+1) + 4*(state.pos.X+1) + state.dir
 
-	return res
+	return 1000*(faces[state.face].Y.Min+state.pos.Y+1) + 4*(faces[state.face].X.Min+state.pos.X+1) + state.dir
 
 }
 
 func main() {
 	fmt.Println("--2022 day 22 solution--")
 	start := time.Now()
-	//fmt.Println("part1: ", Part1(input_day))
+	fmt.Println("part1: ", Part1(input_day))
 	fmt.Println(time.Since(start))
 
 	start = time.Now()

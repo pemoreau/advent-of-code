3ff0e6c054a32a9f7dc8c7cea875a88def85a2ce
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2022-12-25 15:20:29 +0100

    day25

---
+++ go/2022/25/day25_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+)
+
+//go:embed input_test.txt
+var input_test string
+
+func TestPart1(t *testing.T) {
+	result := Part1(string(input_test))
+	expected := "2=-1=0"
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %s, want: %s.", result, expected)
+	}
+}
+
+func TestPart1Input(t *testing.T) {
+	result := Part1(string(input_day))
+	expected := "20-==01-2-=1-2---1-0"
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %s, want: %s.", result, expected)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Part1(input_day)
+	}
+}

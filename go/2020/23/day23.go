4b978dbe6a469896afb93237f895139019cc555c
pem <pierre-etienne.moreau@loria.fr>
2023-02-14 18:52:26 +0100

    start day23

---
+++ go/2020/23/day23.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func Part1(input int) int {
+
+	return 0
+}
+
+func Part2(input int) int {
+	return 0
+}
+
+func main() {
+	fmt.Println("--2020 day 23 solution--")
+	input_day := 156794823
+	start := time.Now()
+	fmt.Println("part1: ", Part1(input_day))
+	fmt.Println(time.Since(start))
+
+	start = time.Now()
+	fmt.Println("part2: ", Part2(input_day))
+	fmt.Println(time.Since(start))
+}
---
+++ go/2020/23/day23_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"testing"
+)
+
+var input_test = 389125467
+var input_day = 156794823
+
+func TestPart1(t *testing.T) {
+	result := Part1(input_test)
+	expected := 306
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(input_test)
+	expected := 291
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart1Input(t *testing.T) {
+	result := Part1(input_day)
+	expected := 30197
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart2Input(t *testing.T) {
+	result := Part2(input_day)
+	expected := 34031
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Part1(input_day)
+	}
+}
+func BenchmarkPart2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Part2(input_day)
+	}
+}

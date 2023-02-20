ef53a47b5492239c9ca496a123181019dc8a3436
pem <pierre-etienne.moreau@loria.fr>
2023-02-20 17:46:37 +0100

    day5

--- rust/2019/day05/src/lib.rs
+++ rust/2019/day05/src/lib.rs
@@ -8,40 +8,96 @@ fn decode(instr: i64) -> (i64, i64, i64, i64) {
     (opcode, mode1, mode2, mode3)
 }
 
-pub fn intcode(input: &mut Vec<i64>) {
+pub fn intcode(program: &mut Vec<i64>, input: i64) {
     let mut pc = 0;
     loop {
-        let (opcode, mode1, mode2, mode3) = decode(input[pc]);
+        let (opcode, mode1, mode2, mode3) = decode(program[pc]);
         // println!("{} : {} {} {} {}", input[pc], opcode, mode1, mode2, mode3);
         match opcode {
             1 => {
-                let a = input[pc + 1] as usize;
-                let arg1 = if mode1 == 0 { input[a] } else { a as i64 };
-                let b = input[pc + 2] as usize;
-                let arg2 = if mode2 == 0 { input[b] } else { b as i64 };
-                let c = input[pc + 3] as usize;
-                input[c] = arg1 + arg2;
+                // add
+                let a = program[pc + 1] as usize;
+                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
+                let b = program[pc + 2] as usize;
+                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                let c = program[pc + 3] as usize;
+                program[c] = arg1 + arg2;
                 pc += 4;
             }
             2 => {
-                let a = input[pc + 1] as usize;
-                let arg1 = if mode1 == 0 { input[a] } else { a as i64 };
-                let b = input[pc + 2] as usize;
-                let arg2 = if mode2 == 0 { input[b] } else { b as i64 };
-                let c = input[pc + 3] as usize;
-                input[c] = arg1 * arg2;
+                // mul
+                let a = program[pc + 1] as usize;
+                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
+                let b = program[pc + 2] as usize;
+                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                let c = program[pc + 3] as usize;
+                program[c] = arg1 * arg2;
                 pc += 4;
             }
             3 => {
-                let a = input[pc + 1] as usize;
-                input[a] = 1; // 1 as input
+                // input
+                let a = program[pc + 1] as usize;
+                program[a] = input;
                 pc += 2;
             }
             4 => {
-                let a = input[pc + 1] as usize;
-                let arg1 = if mode1 == 0 { input[a] } else { a as i64 };
+                // output
+                let a = program[pc + 1] as usize;
+                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
                 println!("output: {}", arg1);
                 pc += 2;
+            }
+            5 => {
+                // jump-if-true
+                let a = program[pc + 1] as usize;
+                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
+                let b = program[pc + 2] as usize;
+                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                if arg1 != 0 {
+                    pc = arg2 as usize;
+                } else {
+                    pc += 3;
+                }
+            }
+            6 => {
+                // jump-if-false
+                let a = program[pc + 1] as usize;
+                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
+                let b = program[pc + 2] as usize;
+                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                if arg1 == 0 {
+                    pc = arg2 as usize;
+                } else {
+                    pc += 3;
+                }
+            }
+            7 => {
+                // less than
+                let a = program[pc + 1] as usize;
+                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
+                let b = program[pc + 2] as usize;
+                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                let c = program[pc + 3] as usize;
+                if arg1 < arg2 {
+                    program[c] = 1;
+                } else {
+                    program[c] = 0;
+                }
+                pc += 4;
+            }
+            8 => {
+                // equals
+                let a = program[pc + 1] as usize;
+                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
+                let b = program[pc + 2] as usize;
+                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                let c = program[pc + 3] as usize;
+                if arg1 == arg2 {
+                    program[c] = 1;
+                } else {
+                    program[c] = 0;
+                }
+                pc += 4;
             }
             99 => break,
             _ => panic!("Unknown opcode"),
@@ -51,10 +107,12 @@ pub fn intcode(input: &mut Vec<i64>) {
 
 pub fn part1(input: String) -> i64 {
     let mut code = comma_separated_to_numbers(input);
-    intcode(&mut code);
-    code[0]
+    intcode(&mut code, 1);
+    0
 }
 
 pub fn part2(input: String) -> i64 {
+    let mut code = comma_separated_to_numbers(input);
+    intcode(&mut code, 5);
     0
 }

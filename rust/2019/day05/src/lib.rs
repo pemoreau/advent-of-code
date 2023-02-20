d305ce0d2cbe1e26219853381d8219d7ba7b2788
pem <pierre-etienne.moreau@loria.fr>
2023-02-20 18:09:22 +0100

    day5

--- rust/2019/day05/src/lib.rs
+++ rust/2019/day05/src/lib.rs
@@ -1,35 +1,42 @@
 use utils::parsing::comma_separated_to_numbers;
 
-fn decode(instr: i64) -> (i64, i64, i64, i64) {
+fn decode(instr: i64) -> (usize, usize, usize, usize) {
     let opcode = instr % 100;
     let mode1 = (instr / 100) % 10;
     let mode2 = (instr / 1000) % 10;
     let mode3 = (instr / 10000) % 10;
-    (opcode, mode1, mode2, mode3)
+    (
+        opcode as usize,
+        mode1 as usize,
+        mode2 as usize,
+        mode3 as usize,
+    )
 }
 
-pub fn intcode(program: &mut Vec<i64>, input: i64) {
+fn decode_args(program: &Vec<i64>, pc: usize, mode1: usize, mode2: usize) -> (i64, i64) {
+    let a = program[pc + 1] as usize;
+    let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
+    let b = program[pc + 2] as usize;
+    let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+    (arg1, arg2)
+}
+
+pub fn intcode(program: &mut Vec<i64>, input: i64, output: &mut i64) {
     let mut pc = 0;
     loop {
-        let (opcode, mode1, mode2, mode3) = decode(program[pc]);
+        let (opcode, mode1, mode2, _) = decode(program[pc]);
         // println!("{} : {} {} {} {}", input[pc], opcode, mode1, mode2, mode3);
         match opcode {
             1 => {
                 // add
-                let a = program[pc + 1] as usize;
-                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
-                let b = program[pc + 2] as usize;
-                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
                 let c = program[pc + 3] as usize;
                 program[c] = arg1 + arg2;
                 pc += 4;
             }
             2 => {
                 // mul
-                let a = program[pc + 1] as usize;
-                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
-                let b = program[pc + 2] as usize;
-                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
                 let c = program[pc + 3] as usize;
                 program[c] = arg1 * arg2;
                 pc += 4;
@@ -45,14 +52,12 @@ pub fn intcode(program: &mut Vec<i64>, input: i64) {
                 let a = program[pc + 1] as usize;
                 let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
                 println!("output: {}", arg1);
+                *output = arg1;
                 pc += 2;
             }
             5 => {
                 // jump-if-true
-                let a = program[pc + 1] as usize;
-                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
-                let b = program[pc + 2] as usize;
-                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
                 if arg1 != 0 {
                     pc = arg2 as usize;
                 } else {
@@ -61,10 +66,7 @@ pub fn intcode(program: &mut Vec<i64>, input: i64) {
             }
             6 => {
                 // jump-if-false
-                let a = program[pc + 1] as usize;
-                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
-                let b = program[pc + 2] as usize;
-                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
                 if arg1 == 0 {
                     pc = arg2 as usize;
                 } else {
@@ -73,10 +75,7 @@ pub fn intcode(program: &mut Vec<i64>, input: i64) {
             }
             7 => {
                 // less than
-                let a = program[pc + 1] as usize;
-                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
-                let b = program[pc + 2] as usize;
-                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
                 let c = program[pc + 3] as usize;
                 if arg1 < arg2 {
                     program[c] = 1;
@@ -87,10 +86,7 @@ pub fn intcode(program: &mut Vec<i64>, input: i64) {
             }
             8 => {
                 // equals
-                let a = program[pc + 1] as usize;
-                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
-                let b = program[pc + 2] as usize;
-                let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
+                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
                 let c = program[pc + 3] as usize;
                 if arg1 == arg2 {
                     program[c] = 1;
@@ -107,12 +103,14 @@ pub fn intcode(program: &mut Vec<i64>, input: i64) {
 
 pub fn part1(input: String) -> i64 {
     let mut code = comma_separated_to_numbers(input);
-    intcode(&mut code, 1);
-    0
+    let mut output = 0;
+    intcode(&mut code, 1, &mut output);
+    output
 }
 
 pub fn part2(input: String) -> i64 {
     let mut code = comma_separated_to_numbers(input);
-    intcode(&mut code, 5);
-    0
+    let mut output = 0;
+    intcode(&mut code, 5, &mut output);
+    output
 }
--- rust/2019/day05/src/tests.rs
+++ rust/2019/day05/src/tests.rs
@@ -3,11 +3,11 @@ mod tests {
 
     #[test]
     fn test_input_part1() {
-        assert_eq!(2692315, part1(include_str!("../input.txt").to_string()));
+        assert_eq!(16574641, part1(include_str!("../input.txt").to_string()));
     }
 
     #[test]
     fn test_input_part2() {
-        assert_eq!(9507, part2(include_str!("../input.txt").to_string()));
+        assert_eq!(15163975, part2(include_str!("../input.txt").to_string()));
     }
 }

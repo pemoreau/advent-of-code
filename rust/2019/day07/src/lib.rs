69ed6547f0c5c1bddab34d3636baeb4201e60f7b
pem <pierre-etienne.moreau@loria.fr>
2023-02-22 12:06:07 +0100

    day 07 part2

--- rust/2019/day07/src/lib.rs
+++ rust/2019/day07/src/lib.rs
@@ -1,17 +1,21 @@
 use itertools::Itertools;
 use utils::parsing::comma_separated_to_numbers;
 
+#[derive(Debug, PartialEq)]
 enum State {
     Running,
     Halted,
     Suspended,
 }
 
+#[derive(Debug)]
 struct Machine {
     pc: usize,
     memory: Vec<i64>,
     input: Vec<i64>,
     output: Vec<i64>,
+    state: State,
+    out: bool,
 }
 
 impl Machine {
@@ -21,19 +25,29 @@ impl Machine {
             memory,
             input,
             output,
+            state: State::Running,
+            out: false,
         }
     }
 
     pub fn run(&mut self) -> Vec<i64> {
         loop {
-            let state = self.step();
-            match state {
+            self.step();
+            match self.state {
                 State::Running => {}
                 State::Halted => break,
                 State::Suspended => break,
             }
         }
         self.output.clone()
+    }
+
+    fn get_last_output(&self) -> i64 {
+        self.output.last().unwrap().clone()
+    }
+
+    fn put_input(&mut self, input: i64) {
+        self.input.push(input);
     }
 
     fn get_opcode(&self) -> usize {
@@ -65,7 +79,12 @@ impl Machine {
         (arg1, arg2)
     }
 
-    fn step(&mut self) -> State {
+    fn step(&mut self) {
+        self.out = false;
+        if self.state == State::Halted {
+            return;
+        }
+
         let opcode = self.get_opcode();
         match opcode {
             1 => {
@@ -86,7 +105,8 @@ impl Machine {
                 // input
                 let a = self.memory[self.pc + 1] as usize;
                 if self.input.is_empty() {
-                    return State::Suspended;
+                    self.state = State::Suspended;
+                    return;
                 }
                 self.memory[a] = self.input.remove(0);
                 self.pc += 2;
@@ -95,6 +115,7 @@ impl Machine {
                 // output
                 let arg1 = self.decode_arg1();
                 self.output.push(arg1);
+                self.out = true;
                 self.pc += 2;
             }
             5 => {
@@ -139,133 +160,64 @@ impl Machine {
             }
             99 => {
                 // halt
-                return State::Halted;
+                self.state = State::Halted;
+                return;
             }
             _ => panic!("Unknown opcode"),
         }
-        State::Running
+        self.state = State::Running;
     }
 }
-
-/*fn decode(instr: i64) -> (usize, usize, usize, usize) {
-    let opcode = instr % 100;
-    let mode1 = (instr / 100) % 10;
-    let mode2 = (instr / 1000) % 10;
-    let mode3 = (instr / 10000) % 10;
-    (
-        opcode as usize,
-        mode1 as usize,
-        mode2 as usize,
-        mode3 as usize,
-    )
-}
-
-fn decode_args(program: &Vec<i64>, pc: usize, mode1: usize, mode2: usize) -> (i64, i64) {
-    let a = program[pc + 1] as usize;
-    let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
-    let b = program[pc + 2] as usize;
-    let arg2 = if mode2 == 0 { program[b] } else { b as i64 };
-    (arg1, arg2)
-}
-
-pub fn intcode(program: &mut Vec<i64>, input: Vec<i64>, output: &mut Vec<i64>) {
-    let mut pc = 0;
-    let mut ic = 0;
-    loop {
-        let (opcode, mode1, mode2, _) = decode(program[pc]);
-        match opcode {
-            1 => {
-                // add
-                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
-                let c = program[pc + 3] as usize;
-                program[c] = arg1 + arg2;
-                pc += 4;
-            }
-            2 => {
-                // mul
-                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
-                let c = program[pc + 3] as usize;
-                program[c] = arg1 * arg2;
-                pc += 4;
-            }
-            3 => {
-                // input
-                let a = program[pc + 1] as usize;
-                program[a] = input[ic];
-                ic += 1;
-                pc += 2;
-            }
-            4 => {
-                // output
-                let a = program[pc + 1] as usize;
-                let arg1 = if mode1 == 0 { program[a] } else { a as i64 };
-                output.push(arg1);
-                pc += 2;
-            }
-            5 => {
-                // jump-if-true
-                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
-                if arg1 != 0 {
-                    pc = arg2 as usize;
-                } else {
-                    pc += 3;
-                }
-            }
-            6 => {
-                // jump-if-false
-                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
-                if arg1 == 0 {
-                    pc = arg2 as usize;
-                } else {
-                    pc += 3;
-                }
-            }
-            7 => {
-                // less than
-                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
-                let c = program[pc + 3] as usize;
-                if arg1 < arg2 {
-                    program[c] = 1;
-                } else {
-                    program[c] = 0;
-                }
-                pc += 4;
-            }
-            8 => {
-                // equals
-                let (arg1, arg2) = decode_args(program, pc, mode1, mode2);
-                let c = program[pc + 3] as usize;
-                if arg1 == arg2 {
-                    program[c] = 1;
-                } else {
-                    program[c] = 0;
-                }
-                pc += 4;
-            }
-            99 => break,
-            _ => panic!("Unknown opcode"),
-        }
-    }
-}*/
 
 fn run_amplifiers(program: &Vec<i64>, phases: Vec<&i64>) -> i64 {
     let mut last_output = 0;
     for phase in phases {
         let code = program.clone();
-        let input = vec![*phase, last_output];
-        let output = Vec::new();
+        let mut input = vec![*phase, last_output];
+        let mut output = Vec::new();
         let mut amp = Machine::new(code, input, output);
-        last_output = amp.run().last().unwrap().clone();
+        amp.run();
+        last_output = amp.get_last_output()
     }
     last_output
 }
 
-pub fn part1(input: String) -> i64 {
+fn run_amplifiers2(program: &Vec<i64>, phases: Vec<&i64>) -> i64 {
+    let n = phases.len();
+
+    // init amps
+    let mut amps = Vec::new();
+    for phase in phases {
+        let code = program.clone();
+        let amp = Machine::new(code, vec![*phase], vec![]);
+        amps.push(amp);
+    }
+    amps[0].put_input(0);
+
+    loop {
+        if amps[n - 1].state == State::Halted {
+            return amps[n - 1].get_last_output();
+        }
+
+        for i in 0..n {
+            amps[i].step();
+            if amps[i].out {
+                let output = amps[i].get_last_output();
+                amps[(i + 1) % n].put_input(output);
+            }
+        }
+    }
+}
+
+fn search_max_signal(
+    input: String,
+    phase_setting: Vec<i64>,
+    run_func: &dyn Fn(&Vec<i64>, Vec<&i64>) -> i64,
+) -> i64 {
     let code = comma_separated_to_numbers(input);
-    let phase_setting = vec![0, 1, 2, 3, 4];
     let mut max_signal = 0;
     for phase in phase_setting.iter().permutations(5) {
-        let signal = run_amplifiers(&code, phase);
+        let signal = run_func(&code, phase);
         if signal > max_signal {
             max_signal = signal;
         }
@@ -273,42 +225,10 @@ pub fn part1(input: String) -> i64 {
     max_signal
 }
 
-// fn run_amplifiers2(program: &Vec<i64>, phases: Vec<&i64>) -> i64 {
-//     let mut last_output = 0;
-//     // init amps
-//     let mut amps = Vec::new();
-//     for phase in phases {
-//         let code = program.clone();
-//         let input = vec![*phase];
-//         let output = Vec::new();
-//         let mut amp = Machine::new(code, input, output);
-//         amps.push(amp);
-//     }
-//     loop {
-//         for amp in amps.iter_mut() {
-//             amp.input.push(last_output);
-//             let output = amp.run();
-//             last_output = output.last().unwrap().clone();
-//         }
-//         if amps.last().unwrap().halted {
-//             break;
-//         }
-//     }
-//     for phase in phases {
-//         let code = program.clone();
-//         let input = vec![*phase, last_output];
-
-//         let mut amp = Machine::new(code, input);
-//         let output = amp.run();
-//         last_output = output.last().unwrap().clone();
-//     }
-//     last_output
-// }
+pub fn part1(input: String) -> i64 {
+    search_max_signal(input, vec![0, 1, 2, 3, 4], &run_amplifiers)
+}
 
 pub fn part2(input: String) -> i64 {
-    let code =
-        "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
-    let phase = vec![9, 8, 7, 6, 5];
-
-    0
+    search_max_signal(input, vec![5, 6, 7, 8, 9], &run_amplifiers2)
 }
--- rust/2019/day07/src/tests.rs
+++ rust/2019/day07/src/tests.rs
@@ -22,14 +22,27 @@ mod tests {
             )
         );
     }
+    #[test]
+    fn test_part2() {
+        assert_eq!(
+            139629729,
+            part2("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5".to_string())
+        );
+        assert_eq!(
+            18216,
+            part2(
+                "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10".to_string()
+            )
+        );
+    }
 
     #[test]
     fn test_input_part1() {
         assert_eq!(398674, part1(include_str!("../input.txt").to_string()));
     }
 
-    // #[test]
-    // fn test_input_part2() {
-    //     assert_eq!(15163975, part2(include_str!("../input.txt").to_string()));
-    // }
+    #[test]
+    fn test_input_part2() {
+        assert_eq!(39431233, part2(include_str!("../input.txt").to_string()));
+    }
 }

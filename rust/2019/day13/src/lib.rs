6fc473019ccfb33609d5bead65b3f8ed164ee8d4
pem <pierre-etienne.moreau@loria.fr>
2023-03-13 18:21:27 +0100

    day13

--- rust/2019/day13/src/lib.rs
+++ rust/2019/day13/src/lib.rs
@@ -46,7 +46,7 @@ impl Cabinet {
         let max_x = self.grid.keys().map(|p| p.x).max().unwrap();
         let min_y = self.grid.keys().map(|p| p.y).min().unwrap();
         let max_y = self.grid.keys().map(|p| p.y).max().unwrap();
-        for y in (min_y..=max_y) {
+        for y in min_y..=max_y {
             for x in min_x..=max_x {
                 if x == self.ball.x && y == self.ball.y {
                     print!("o");
@@ -66,53 +66,50 @@ impl Cabinet {
             println!();
         }
     }
-
-    fn game_over(&self) -> bool {
-        let min_x = self.grid.keys().map(|p| p.x).min().unwrap();
-        let max_x = self.grid.keys().map(|p| p.x).max().unwrap();
-        let min_y = self.grid.keys().map(|p| p.y).min().unwrap();
-        let max_y = self.grid.keys().map(|p| p.y).max().unwrap();
-
-        !(min_x <= self.ball.x
-            && self.ball.x <= max_x
-            && min_y <= self.ball.y
-            && self.ball.y <= max_y)
-    }
-
-    fn step(&mut self) {
-        // move ball
-        let next = Pos {
-            x: self.ball.x + self.dir.x,
-            y: self.ball.y + self.dir.y,
-        };
-        let ball = self.ball.clone();
-        let tile = self.grid.get(&next).unwrap_or(&0);
-        if *tile == 0 || *tile == 2 {
-            self.grid.insert(ball, 0);
-            self.ball = next;
-        } else if *tile == 1 || *tile == 3 {
-            self.dir.x *= -1;
-            self.dir.y *= -1;
-            self.ball = Pos {
-                x: self.ball.x + self.dir.x,
-                y: self.ball.y + self.dir.y,
-            };
-        }
-    }
 }
 
 pub fn part1(input: String) -> i64 {
     let code = comma_separated_to_numbers(input);
     let mut game = Cabinet::new();
     game.init(code);
-    game.display();
-    // while !game.game_over() {
-    //     game.step();
-    //     game.display();
-    // }
     game.grid.values().filter(|&&t| t == 2).count() as i64
 }
 
 pub fn part2(input: String) -> i64 {
-    0
+    let mut code = comma_separated_to_numbers(input);
+    code[0] = 2;
+    let mut machine = Machine::new(code, vec![]);
+    let mut ball = Pos { x: 0, y: 0 };
+    let mut paddle = Pos { x: 0, y: 0 };
+    let mut score = 0;
+    while !machine.is_halted() {
+        machine.run();
+        let output = machine.get_last_output();
+        for i in 0..output.len() / 3 {
+            let x = output[i * 3];
+            let y = output[i * 3 + 1];
+            let tile = output[i * 3 + 2];
+            if x == -1 && y == 0 {
+                println!("Score: {}", tile);
+                score = tile;
+            } else if tile == 4 {
+                println!("Set ball to {},{}", x, y);
+                ball = Pos { x, y };
+            } else if tile == 3 {
+                println!("Set paddle to {},{}", x, y);
+                paddle = Pos { x, y };
+            }
+        }
+        let dir = if ball.x < paddle.x {
+            -1
+        } else if ball.x > paddle.x {
+            1
+        } else {
+            0
+        };
+        println!("move paddle (x={}) dir {}", paddle.x, dir);
+        machine.put_input(dir)
+    }
+
+    score
 }
--- rust/2019/day13/src/tests.rs
+++ rust/2019/day13/src/tests.rs
@@ -3,11 +3,11 @@ mod tests {
 
     #[test]
     fn test_input_part1() {
-        assert_eq!(1934, part1(include_str!("../input.txt").to_string()));
+        assert_eq!(173, part1(include_str!("../input.txt").to_string()));
     }
 
-    // #[test]
-    // fn test_input_part2() {
-    //     assert_eq!(404, part2(include_str!("../input.txt").to_string()));
-    // }
+    #[test]
+    fn test_input_part2() {
+        assert_eq!(8942, part2(include_str!("../input.txt").to_string()));
+    }
 }

a3f0c7fc3c6732e0c05f67cdb152210a65f9a542
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-04-04 03:40:32 +0200

    day 14

--- rust/2019/day14/src/lib.rs
+++ rust/2019/day14/src/lib.rs
@@ -37,63 +37,108 @@ fn parse(input: String) -> HashMap<Chemical, Vec<Chemical>> {
     reactions
 }
 
-fn update_element(subject: &mut HashMap<String, i64>, n: i64, name: &str) {
+fn update_element(subject: &mut HashMap<String, i64>, n: i64, name: &String) {
     let n = n + subject.get(name).unwrap_or(&0);
-    subject.insert(name.to_string(), n);
+    //subject.insert(name.to_string(), n);
+    if n == 0 {
+        subject.remove(name);
+    } else {
+        subject.insert(name.to_string(), n);
+    }
 }
 
 fn rewrite_step(
     reactions: &HashMap<Chemical, Vec<Chemical>>,
+    strict: bool,
     subject: &mut HashMap<String, i64>,
-) -> (Map<String, i64>, bool) {
-    let mut result = subject.clone();
+    stock: &mut HashMap<String, i64>,
+) -> bool {
     for (element, n) in subject.clone() {
-        for (lhs, rhs) in reactions {
-            if lhs.name == element.clone() && n > 0 && n >= lhs.n {
-                // rule can be applied
-                let factor = n / lhs.n;
-                let remainder = n % lhs.n;
-                println!(
-                    "apply {}*{:?}=>{:?} on {:?}",
-                    factor,
-                    lhs,
-                    rhs,
-                    subject.clone()
-                );
-                if remainder != 0 {
-                    update_element(&mut result, remainder, element.as_str())
+        if n > 0 {
+            if let Some(stock_n) = stock.get(&element) {
+                if *stock_n > 0 {
+                    let n = std::cmp::min(*stock_n, n);
+                    update_element(stock, -n, &element);
+                    update_element(subject, -n, &element);
+                    //println!("use stock {:?} on {:?}", stock, subject);
+                    return true;
                 }
-                // remove lhs
-                update_element(&mut result, -lhs.n * factor, lhs.name.as_str());
-                for Chemical { n, name } in rhs {
-                    for _ in 0..factor {
-                        update_element(&mut result, *n, name);
-                    }
-                }
-                return (result, true);
-            } else {
-                // rule cannot be applied
             }
         }
     }
-    (result, false)
+    for (element, n) in subject.clone() {
+        if n == 0 {
+            continue;
+        }
+        for (lhs, rhs) in reactions {
+            if lhs.name == element.clone() {
+                let factor = n / lhs.n;
+                let remainder = n % lhs.n;
+                if remainder == 0 {
+                    // rule can be applied
+                    //println!("apply {:?} => {:?} on {:?}", lhs, rhs, subject);
+                    // remove lhs
+                    subject.remove(&lhs.name);
+                    // add rhs
+                    for Chemical { n, name } in rhs {
+                        update_element(subject, factor * n, name);
+                    }
+                    return true;
+                } else {
+                    // rule can be applied
+                    //println!("apply+1 {:?} => {:?} on {:?}", lhs, rhs, subject);
+                    // remove lhs
+                    subject.remove(&lhs.name);
+                    update_element(stock, lhs.n - remainder, &lhs.name);
+                    // add rhs
+                    for Chemical { n, name } in rhs {
+                        update_element(subject, (factor + 1) * n, name);
+                    }
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
+
+fn fuel1(reactions: &HashMap<Chemical, Vec<Chemical>>, goal: i64) -> i64 {
+    let mut subject = HashMap::new();
+    let mut stock = HashMap::new();
+    subject.insert("FUEL".to_string(), goal);
+    let mut modified = true;
+    while modified {
+        modified = rewrite_step(&reactions, true, &mut subject, &mut stock);
+        //println!("subject={:?}, stock={:?}", subject, stock);
+    }
+    //println!("FINAL subject={:?}, stock={:?}", subject, stock);
+
+    subject.get("ORE").unwrap().clone()
 }
 
 pub fn part1(input: String) -> i64 {
     let reactions = parse(input);
-    let mut subject = HashMap::new();
-    subject.insert("FUEL".to_string(), 1);
-    let mut modified = true;
-    while modified {
-        let res = rewrite_step(&reactions, subject.clone());
-        modified = res.1;
-        subject = res.0;
-        println!("{:?}", subject);
-    }
-
-    0
+    fuel1(&reactions, 1)
 }
 
 pub fn part2(input: String) -> i64 {
-    0
+    let NB_ORE = 1000000000000;
+    let reactions = parse(input);
+    let cost_fuel1 = fuel1(&reactions, 1);
+    let mut a = NB_ORE / cost_fuel1;
+    let mut b = 3 * a;
+    let mut oa = fuel1(&reactions, a);
+    let mut ob = fuel1(&reactions, b);
+    while b - a > 1 {
+        let c = (a + b) / 2;
+        let oc = fuel1(&reactions, c);
+        if oc > NB_ORE {
+            b = c;
+            ob = oc;
+        } else {
+            a = c;
+            oa = oc;
+        }
+    }
+    a
 }

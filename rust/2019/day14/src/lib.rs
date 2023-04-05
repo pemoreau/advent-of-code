fc864a31666ef252c6d941a7686ecb015698c999
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-04-05 19:19:13 +0200

    improve day 14

--- rust/2019/day14/src/lib.rs
+++ rust/2019/day14/src/lib.rs
@@ -6,19 +6,29 @@ struct Chemical {
     name: String,
 }
 
-fn parse(input: String) -> HashMap<Chemical, Vec<Chemical>> {
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct Reaction {
+    name: String,
+    n: i64,
+    rhs: Vec<Chemical>,
+}
+
+type Reactions = HashMap<String, Reaction>;
+type Elements = HashMap<String, i64>;
+
+fn parse(input: String) -> Reactions {
     let mut reactions = HashMap::new();
     for line in input.lines() {
         let mut parts = line.split("=>");
         let lhs = parts.next().unwrap();
         let rhs = parts.next().unwrap().trim();
-        let mut rhs_parts = rhs.split(" ");
+        let mut rhs_parts = rhs.split(' ');
         let rhs_n = rhs_parts.next().unwrap().parse::<i64>().unwrap();
         let rhs_name = rhs_parts.next().unwrap().to_string();
-        let lhs_parts = lhs.split(",");
+        let lhs_parts = lhs.split(',');
         let mut lhs_vec = Vec::new();
         for chem in lhs_parts {
-            let mut chem_parts = chem.trim().split(" ");
+            let mut chem_parts = chem.trim().split(' ');
             let chem_n = chem_parts.next().unwrap().parse::<i64>().unwrap();
             let chem_name = chem_parts.next().unwrap().to_string();
             lhs_vec.push(Chemical {
@@ -26,18 +36,18 @@ fn parse(input: String) -> HashMap<Chemical, Vec<Chemical>> {
                 name: chem_name,
             });
         }
-        reactions.insert(
-            Chemical {
-                n: rhs_n,
-                name: rhs_name,
-            },
-            lhs_vec,
-        );
+        let reaction = Reaction {
+            name: rhs_name.clone(),
+            n: rhs_n,
+            rhs: lhs_vec,
+        };
+
+        reactions.insert(rhs_name, reaction);
     }
     reactions
 }
 
-fn update_element(subject: &mut HashMap<String, i64>, n: i64, name: &String) {
+fn update_element(subject: &mut Elements, n: i64, name: &String) {
     let n = n + subject.get(name).unwrap_or(&0);
     //subject.insert(name.to_string(), n);
     if n == 0 {
@@ -47,12 +57,7 @@ fn update_element(subject: &mut HashMap<String, i64>, n: i64, name: &String) {
     }
 }
 
-fn rewrite_step(
-    reactions: &HashMap<Chemical, Vec<Chemical>>,
-    strict: bool,
-    subject: &mut HashMap<String, i64>,
-    stock: &mut HashMap<String, i64>,
-) -> bool {
+fn rewrite_step(reactions: &Reactions, subject: &mut Elements, stock: &mut Elements) -> bool {
     for (element, n) in subject.clone() {
         if n > 0 {
             if let Some(stock_n) = stock.get(&element) {
@@ -70,50 +75,43 @@ fn rewrite_step(
         if n == 0 {
             continue;
         }
-        for (lhs, rhs) in reactions {
-            if lhs.name == element.clone() {
-                let factor = n / lhs.n;
-                let remainder = n % lhs.n;
-                if remainder == 0 {
-                    // rule can be applied
-                    //println!("apply {:?} => {:?} on {:?}", lhs, rhs, subject);
-                    // remove lhs
-                    subject.remove(&lhs.name);
-                    // add rhs
-                    for Chemical { n, name } in rhs {
-                        update_element(subject, factor * n, name);
-                    }
-                    return true;
+        if reactions.contains_key(&element) {
+            let reaction = reactions.get(&element).unwrap();
+            if reaction.name == element.clone() {
+                let remainder = n % reaction.n;
+                let factor = if remainder == 0 {
+                    n / reaction.n
                 } else {
-                    // rule can be applied
-                    //println!("apply+1 {:?} => {:?} on {:?}", lhs, rhs, subject);
-                    // remove lhs
-                    subject.remove(&lhs.name);
-                    update_element(stock, lhs.n - remainder, &lhs.name);
-                    // add rhs
-                    for Chemical { n, name } in rhs {
-                        update_element(subject, (factor + 1) * n, name);
-                    }
-                    return true;
+                    n / reaction.n + 1
+                };
+                // remove lhs
+                subject.remove(&reaction.name);
+                // add rhs
+                for Chemical { n, name } in &reaction.rhs {
+                    update_element(subject, factor * n, name);
                 }
+                if remainder != 0 {
+                    update_element(stock, reaction.n - remainder, &reaction.name);
+                }
+                return true;
             }
         }
     }
     false
 }
 
-fn fuel1(reactions: &HashMap<Chemical, Vec<Chemical>>, goal: i64) -> i64 {
+fn fuel1(reactions: &Reactions, goal: i64) -> i64 {
     let mut subject = HashMap::new();
     let mut stock = HashMap::new();
     subject.insert("FUEL".to_string(), goal);
     let mut modified = true;
     while modified {
-        modified = rewrite_step(&reactions, true, &mut subject, &mut stock);
+        modified = rewrite_step(reactions, &mut subject, &mut stock);
         //println!("subject={:?}, stock={:?}", subject, stock);
     }
     //println!("FINAL subject={:?}, stock={:?}", subject, stock);
 
-    subject.get("ORE").unwrap().clone()
+    *subject.get("ORE").unwrap()
 }
 
 pub fn part1(input: String) -> i64 {
@@ -122,22 +120,18 @@ pub fn part1(input: String) -> i64 {
 }
 
 pub fn part2(input: String) -> i64 {
-    let NB_ORE = 1000000000000;
+    let nb_ore = 1000000000000;
     let reactions = parse(input);
     let cost_fuel1 = fuel1(&reactions, 1);
-    let mut a = NB_ORE / cost_fuel1;
+    let mut a = nb_ore / cost_fuel1;
     let mut b = 3 * a;
-    let mut oa = fuel1(&reactions, a);
-    let mut ob = fuel1(&reactions, b);
     while b - a > 1 {
         let c = (a + b) / 2;
         let oc = fuel1(&reactions, c);
-        if oc > NB_ORE {
+        if oc > nb_ore {
             b = c;
-            ob = oc;
         } else {
             a = c;
-            oa = oc;
         }
     }
     a

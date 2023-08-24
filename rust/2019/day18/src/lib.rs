b21ed539158fb152098b85b088083f2e8d75cb3d
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-08-24 08:40:40 +0200

    start part 1

--- rust/2019/day18/src/lib.rs
+++ rust/2019/day18/src/lib.rs
@@ -1,44 +1,66 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+struct Pos {
+    x: i64,
+    y: i64,
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+struct Node {
+    pos: Pos,
+    name: char,
+}
 
 #[derive(Debug, Eq, PartialEq, Hash)]
 struct Edge {
-    to: char,
+    to: Node,
     distance: u32,
 }
 
 impl Edge {
-    fn new(to: char, distance: u32) -> Self {
+    fn new(to: Node, distance: u32) -> Self {
         Self { to, distance }
     }
 }
 
 #[derive(Debug)]
 struct State {
-    node: char,
+    current: Node,
     keys: Vec<char>,
+    path: Vec<char>,
     cost: u32,
 }
 
-type Graph = HashMap<char, Vec<Edge>>;
+type Graph = HashMap<Node, Vec<Edge>>;
 
 fn neighboors(graph: &Graph, state: &State) -> Vec<State> {
     let mut neighboors = Vec::new();
-    for Edge { to, distance } in graph.get(&state.node).unwrap() {
-        if to.is_ascii_lowercase() {
+    let mut path = state.path.clone();
+    for Edge { to, distance } in graph.get(&state.current).unwrap() {
+        if to.name.is_ascii_lowercase() {
             let mut keys = state.keys.clone();
-            if !keys.contains(to) {
-                keys.push(*to);
+            if !keys.contains(&to.name) {
+                keys.push(to.name);
                 keys.sort();
             }
+            path.push(to.name);
             neighboors.push(State {
-                node: *to,
+                current: to.clone(),
                 keys: keys,
+                path: path.clone(),
                 cost: state.cost + distance,
             });
-        } else if state.keys.contains(&to.to_ascii_lowercase()) || !to.is_ascii_uppercase() {
+        } else if state.keys.contains(&to.name.to_ascii_lowercase())
+            || !to.name.is_ascii_uppercase()
+        {
+            if to.name.is_ascii_uppercase() {
+                path.push(to.name);
+            }
             neighboors.push(State {
-                node: *to,
+                current: to.clone(),
                 keys: state.keys.clone(),
+                path: path.clone(),
                 cost: state.cost + distance,
             });
         }
@@ -46,30 +68,37 @@ fn neighboors(graph: &Graph, state: &State) -> Vec<State> {
     neighboors
 }
 
-fn bfs(graph: &Graph, start: char) -> u32 {
+fn bfs(graph: &Graph, start: Node, number_of_keys: usize) -> u32 {
+    let mut min_cost = u32::MAX;
     let mut queue = Vec::new();
-    let mut visited = HashMap::new();
+    let mut visited: HashMap<(Node, Vec<char>), u32> = HashMap::new();
     queue.push(State {
-        node: start,
+        current: start,
         keys: Vec::new(),
+        path: Vec::new(),
         cost: 0,
     });
     while !queue.is_empty() {
         let state = queue.remove(0);
         println!("{:?}", state);
-        if visited.contains_key(&(state.node, state.keys.clone())) {
-            let cost = visited.get(&(state.node, state.keys.clone())).unwrap();
+        if visited.contains_key(&(state.current.clone(), state.keys.clone())) {
+            let cost = visited
+                .get(&(state.current.clone(), state.keys.clone()))
+                .unwrap();
             if state.cost >= *cost {
                 println!("skip {:?} {:?}", state, cost);
                 continue;
             }
         }
-        if state.keys.len() == 7 {
-            println!("Found all keys in {} steps", state.cost);
+        if state.keys.len() == number_of_keys {
+            if state.cost < min_cost {
+                println!("Found all keys: {:?}", state);
+                min_cost = state.cost;
+            }
             continue;
         }
 
-        visited.insert((state.node, state.keys.clone()), state.cost);
+        visited.insert((state.current.clone(), state.keys.clone()), state.cost);
         let neighboors = neighboors(graph, &state);
         println!("neighboors {:?}", neighboors);
         for neighboor in neighboors {
@@ -77,31 +106,158 @@ fn bfs(graph: &Graph, start: char) -> u32 {
             queue.push(neighboor);
         }
     }
-    *visited.values().min().unwrap()
+    min_cost
 }
 
 // https://kuczma.dev/articles/rust-graphs/
 
-pub fn part1(input: String) -> i64 {
-    // create nodes: f-D-C-b-@-a-B-c-d-A-e-F-g
-    let mut graph: Graph = HashMap::new();
-    graph.insert('f', vec![Edge::new('D', 2)]);
-    graph.insert('D', vec![Edge::new('C', 2), Edge::new('f', 2)]);
-    graph.insert('C', vec![Edge::new('b', 2), Edge::new('D', 2)]);
-    graph.insert('b', vec![Edge::new('@', 22), Edge::new('C', 2)]);
-    graph.insert('@', vec![Edge::new('a', 2), Edge::new('b', 22)]);
-    graph.insert('a', vec![Edge::new('B', 2), Edge::new('@', 2)]);
-    graph.insert('B', vec![Edge::new('c', 2), Edge::new('a', 2)]);
-    graph.insert('c', vec![Edge::new('d', 2), Edge::new('B', 2)]);
-    graph.insert('d', vec![Edge::new('A', 2), Edge::new('c', 2)]);
-    graph.insert('A', vec![Edge::new('e', 2), Edge::new('d', 2)]);
-    graph.insert('e', vec![Edge::new('F', 2), Edge::new('A', 2)]);
-    graph.insert('F', vec![Edge::new('g', 2), Edge::new('e', 2)]);
-    graph.insert('g', vec![Edge::new('F', 2)]);
-
-    bfs(&graph, '@');
-    0
+struct Grid {
+    grid: HashMap<Pos, char>,
 }
+
+impl Grid {
+    fn new() -> Self {
+        Self {
+            grid: HashMap::new(),
+        }
+    }
+
+    fn start_pos(&self) -> Pos {
+        self.grid
+            .iter()
+            .find(|(_, c)| **c == '@')
+            .map(|(pos, _)| pos.clone())
+            .unwrap()
+    }
+
+    fn insert(&mut self, pos: Pos, c: char) {
+        self.grid.insert(pos, c);
+    }
+
+    fn get(&self, pos: &Pos) -> Option<&char> {
+        self.grid.get(pos)
+    }
+
+    fn contains_key(&self, pos: &Pos) -> bool {
+        self.grid.contains_key(pos)
+    }
+
+    fn is_wall(&self, pos: &Pos) -> bool {
+        self.get(pos).unwrap() == &'#'
+    }
+
+    fn neighboors(&self, pos: &Pos) -> Vec<Pos> {
+        let mut neighboors = Vec::new();
+        for (dx, dy) in vec![(0, 1), (0, -1), (1, 0), (-1, 0)] {
+            let new_pos = Pos {
+                x: pos.x + dx,
+                y: pos.y + dy,
+            };
+            if self.contains_key(&new_pos) && !self.is_wall(&new_pos) {
+                neighboors.push(new_pos);
+            }
+        }
+        neighboors
+    }
+
+    fn is_branching_position(&self, pos: &Pos) -> bool {
+        if self.is_wall(&pos) {
+            return false;
+        }
+
+        let c = self.get(&pos).unwrap();
+        c.is_ascii() || self.neighboors(&pos).len() > 2
+    }
+
+    fn number_of_keys(&self) -> usize {
+        self.grid
+            .iter()
+            .filter(|(_, c)| c.is_ascii_lowercase())
+            .count()
+    }
+}
+
+fn is_branch_position(grid: &Grid, pos: &Pos) -> bool {
+    let c = grid.get(&pos).unwrap();
+    if c == &'#' {
+        return false;
+    }
+
+    let mut neighboors = 0;
+    for (dx, dy) in vec![(0, 1), (0, -1), (1, 0), (-1, 0)] {
+        let new_pos = Pos {
+            x: pos.x + dx,
+            y: pos.y + dy,
+        };
+        if grid.contains_key(&new_pos) && grid.get(&new_pos).unwrap() != &'#' {
+            neighboors += 1;
+        }
+    }
+    // println!("branching {} {:?}", neighboors, pos);
+    c.is_ascii_lowercase() || c.is_ascii_uppercase() || neighboors > 2
+}
+
+pub fn part1(input: String) -> i64 {
+    let mut grid = Grid::new();
+    for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            grid.insert(
+                Pos {
+                    x: x as i64,
+                    y: y as i64,
+                },
+                c,
+            );
+        }
+    }
+    let mut graph = HashMap::new();
+    let mut queue = Vec::new();
+    let mut visited: HashSet<Node> = HashSet::new();
+    let start = Node {
+        pos: grid.start_pos(),
+        name: '@',
+    };
+    queue.push((start.clone(), start.clone(), 0));
+    while !queue.is_empty() {
+        let (cell, from, distance) = queue.remove(0);
+        if grid.is_wall(&cell.pos.clone()) {
+            continue;
+        }
+        if visited.contains(&cell.clone()) {
+            continue;
+        }
+        visited.insert(cell.clone());
+        let new_from = if is_branch_position(&grid, &cell.pos.clone()) && cell != from {
+            graph
+                .entry(from.clone())
+                .or_insert(Vec::new())
+                .push(Edge::new(cell.clone(), distance));
+            graph
+                .entry(cell.clone())
+                .or_insert(Vec::new())
+                .push(Edge::new(from.clone(), distance));
+            cell.clone()
+        } else {
+            from.clone()
+        };
+
+        grid.neighboors(&cell.pos).iter().for_each(|neighboor| {
+            queue.push((
+                Node {
+                    pos: neighboor.clone(),
+                    name: *grid.get(&neighboor).unwrap(),
+                },
+                new_from.clone(),
+                if new_from == from { distance + 1 } else { 1 },
+            ))
+        });
+    }
+    println!("graph {:?}", graph);
+    bfs(&graph, start, grid.number_of_keys()) as i64
+}
+
+// 462 too low
+// 5410 too high
 
 pub fn part2(input: String) -> i64 {
     0
--- rust/2019/day18/src/tests.rs
+++ rust/2019/day18/src/tests.rs
@@ -3,6 +3,26 @@ mod tests {
     use crate::part2;
 
     #[test]
+    fn test_part1_1() {
+        assert_eq!(86, part1(include_str!("../input_test1.txt").to_string()));
+    }
+
+    #[test]
+    fn test_part1_2() {
+        assert_eq!(132, part1(include_str!("../input_test2.txt").to_string()));
+    }
+
+    #[test]
+    fn test_part1_3() {
+        assert_eq!(136, part1(include_str!("../input_test3.txt").to_string()));
+    }
+
+    #[test]
+    fn test_part1_4() {
+        assert_eq!(81, part1(include_str!("../input_test4.txt").to_string()));
+    }
+
+    #[test]
     fn test_input_part1() {
         assert_eq!(0, part1(include_str!("../input.txt").to_string()));
     }

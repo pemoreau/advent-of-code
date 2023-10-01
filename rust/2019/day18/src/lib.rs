5fe4c70158d445c6e956f2b47219cc281e9453bb
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-10-01 10:10:22 +0200

    refactor

--- rust/2019/day18/src/lib.rs
+++ rust/2019/day18/src/lib.rs
@@ -12,23 +12,15 @@ struct Node {
     name: char,
 }
 
-#[derive(Debug)]
-struct State {
-    current: Node,
-    keys: Vec<char>,
-    path: Vec<char>,
-    cost: i32,
-}
-
 #[derive(Debug, Hash, Eq, PartialEq, Clone)]
-struct State2 {
+struct State {
     current: Vec<Node>,
     keys: Vec<char>,
 }
 
 #[derive(Debug, Clone)]
 struct StateCost {
-    state: State2,
+    state: State,
     cost: i32,
 }
 
@@ -103,81 +95,6 @@ impl Graph {
     }
 }
 
-fn neighbours(graph: &Graph, state: &State) -> Vec<State> {
-    let mut neighbours = Vec::new();
-    for (to, distance) in graph.neighbours(&state.current).unwrap() {
-        let mut path = state.path.clone();
-        path.push(to.name);
-        if to.name.is_ascii_lowercase() {
-            let mut keys = state.keys.clone();
-            if !keys.contains(&to.name) {
-                keys.push(to.name);
-                keys.sort();
-            }
-            neighbours.push(State {
-                current: to.clone(),
-                keys: keys,
-                path: path,
-                cost: state.cost + distance,
-            });
-        } else if state.keys.contains(&to.name.to_ascii_lowercase())
-            || !to.name.is_ascii_uppercase()
-        {
-            neighbours.push(State {
-                current: to.clone(),
-                keys: state.keys.clone(),
-                path: path,
-                cost: state.cost + distance,
-            });
-        }
-    }
-    neighbours
-}
-
-fn bfs(graph: &Graph, start: Node, number_of_keys: usize) -> i32 {
-    let mut min_cost = i32::MAX;
-    let mut queue = Vec::new();
-    let mut visited: HashMap<(Node, Vec<char>), i32> = HashMap::new();
-    queue.push(State {
-        current: start,
-        keys: Vec::new(),
-        path: Vec::new(),
-        cost: 0,
-    });
-    while !queue.is_empty() {
-        let state = queue.remove(0);
-        // println!("{:?}", state);
-        if visited.contains_key(&(state.current.clone(), state.keys.clone())) {
-            let cost = visited
-                .get(&(state.current.clone(), state.keys.clone()))
-                .unwrap();
-            if state.cost >= *cost {
-                // println!("skip {:?} {:?}", state, cost);
-                continue;
-            }
-        }
-        visited.insert((state.current.clone(), state.keys.clone()), state.cost);
-
-        if state.keys.len() == number_of_keys {
-            if state.cost < min_cost {
-                // println!("Found all keys: {:?}", state);
-                min_cost = state.cost;
-            }
-            continue;
-        }
-
-        let neighboors = neighbours(graph, &state);
-        // println!("neighboors {:?}", neighboors);
-        for neighboor in neighboors {
-            // println!("add {:?}", neighboor);
-            queue.push(neighboor);
-        }
-    }
-    min_cost
-}
-
-// https://kuczma.dev/articles/rust-graphs/
-
 struct Grid {
     grid: HashMap<Pos, char>,
 }
@@ -193,7 +110,7 @@ impl Grid {
         self.grid
             .iter()
             .find(|(_, c)| **c == '@')
-            .map(|(pos, _)| pos.clone())
+            .map(|(pos, _)| *pos)
             .unwrap()
     }
 
@@ -234,62 +151,17 @@ impl Grid {
             .count()
     }
 
-    fn display(&self) {
-        let mut min_x = i64::MAX;
-        let mut max_x = i64::MIN;
-        let mut min_y = i64::MAX;
-        let mut max_y = i64::MIN;
-        for (pos, _) in self.grid.iter() {
-            if pos.x < min_x {
-                min_x = pos.x;
-            }
-            if pos.x > max_x {
-                max_x = pos.x;
-            }
-            if pos.y < min_y {
-                min_y = pos.y;
-            }
-            if pos.y > max_y {
-                max_y = pos.y;
-            }
+    fn is_branch_position(&self, pos: &Pos) -> bool {
+        let &c = self.get(&pos).unwrap();
+        if c == '#' {
+            return false;
         }
-        for y in min_y..=max_y {
-            for x in min_x..=max_x {
-                let pos = Pos { x, y };
-                if self.contains_key(&pos) {
-                    print!("{}", self.get(&pos).unwrap());
-                } else {
-                    print!(" ");
-                }
-            }
-            println!();
-        }
+        c.is_ascii_lowercase() || c.is_ascii_uppercase()
     }
-}
-
-fn is_branch_position(grid: &Grid, pos: &Pos) -> bool {
-    let &c = grid.get(&pos).unwrap();
-    if c == '#' {
-        return false;
-    }
-
-    let mut neighboors = 0;
-    for (dx, dy) in vec![(0, 1), (0, -1), (1, 0), (-1, 0)] {
-        let new_pos = Pos {
-            x: pos.x + dx,
-            y: pos.y + dy,
-        };
-        if grid.contains_key(&new_pos) && grid.get(&new_pos).unwrap() != &'#' {
-            neighboors += 1;
-        }
-    }
-    // println!("branching {} {:?}", neighboors, pos);
-    // c.is_ascii_lowercase() || c.is_ascii_uppercase() || neighboors > 2
-    c.is_ascii_lowercase() || c.is_ascii_uppercase()
 }
 
 fn extend_graph(graph: &mut Graph, grid: &Grid, start: &Node) {
-    let mut queue = Vec::new();
+    let mut queue: Vec<(Node, Node, i32)> = Vec::new();
     let mut visited: HashSet<(Node, Node)> = HashSet::new();
     queue.push((*start, *start, 0));
     while !queue.is_empty() {
@@ -302,22 +174,19 @@ fn extend_graph(graph: &mut Graph, grid: &Grid, start: &Node) {
             continue;
         }
         visited.insert((cell, from));
-        let new_from = if is_branch_position(&grid, &pos) && cell != from {
+        let new_from = if grid.is_branch_position(&pos) && cell != from {
             graph.add_edge((&cell, &from, distance));
             cell
         } else {
             from
         };
 
-        grid.neighboors(&cell.pos).iter().for_each(|neighboor| {
-            queue.push((
-                Node {
-                    pos: neighboor.clone(),
-                    name: *grid.get(&neighboor).unwrap(),
-                },
-                new_from.clone(),
-                if new_from == from { distance + 1 } else { 1 },
-            ))
+        grid.neighboors(&cell.pos).iter().for_each(|p| {
+            let n = Node {
+                pos: *p,
+                name: *grid.get(p).unwrap(),
+            };
+            queue.push((n, new_from, if new_from == from { distance + 1 } else { 1 }))
         });
     }
 }
@@ -346,14 +215,11 @@ pub fn part1(input: String) -> i64 {
     };
     let mut graph = Graph::new();
     extend_graph(&mut graph, &grid, &start);
-    // println!("graph {:?}", graph);
-    // bfs(&graph, start, grid.number_of_keys()) as i64
-    // bfs2(&graph, vec![start], grid.number_of_keys()) as i64
     dijkstra(&grid, &graph, vec![start]) as i64
 }
 
-fn neighbours2(graph: &Graph, state: &State2) -> Vec<(State2, i32)> {
-    let mut neighbours: Vec<(State2, i32)> = Vec::new();
+fn neighbours(graph: &Graph, state: &State) -> Vec<(State, i32)> {
+    let mut res: Vec<(State, i32)> = Vec::new();
     for i in 0..state.current.len() {
         let possible_destinations: Vec<&(Node, i32)> = graph
             .neighbours(&state.current[i])
@@ -363,75 +229,23 @@ fn neighbours2(graph: &Graph, state: &State2) -> Vec<(State2, i32)> {
             .collect();
 
         for (to, distance) in possible_destinations {
+            let mut s = state.clone();
+            s.current[i] = *to;
+            if to.name.is_ascii_uppercase() && !state.keys.contains(&to.name.to_ascii_lowercase()) {
+                continue;
+            }
             if to.name.is_ascii_lowercase() {
-                let mut keys = state.keys.clone();
-                keys.push(to.name);
-                keys.sort();
-                keys.dedup();
-                let mut current = state.current.clone();
-                current[i] = to.clone();
-                neighbours.push((
-                    State2 {
-                        current: current,
-                        keys: keys,
-                        // cost: state.cost + distance,
-                    },
-                    *distance,
-                ));
-            } else if state.keys.contains(&to.name.to_ascii_lowercase())
-                || !to.name.is_ascii_uppercase()
-            {
-                let mut current = state.current.clone();
-                current[i] = to.clone();
-                neighbours.push((
-                    State2 {
-                        current: current,
-                        keys: state.keys.clone(),
-                        // cost: state.cost + distance,
-                    },
-                    *distance,
-                ));
+                s.keys.push(to.name);
+                s.keys.sort();
+                s.keys.dedup();
             }
+            res.push((s, *distance));
         }
     }
-    neighbours
+    res
 }
 
-fn display(grid: &Grid, state: &State2) {
-    let mut min_x = i64::MAX;
-    let mut max_x = i64::MIN;
-    let mut min_y = i64::MAX;
-    let mut max_y = i64::MIN;
-    for node in state.current.iter() {
-        if node.pos.x < min_x {
-            min_x = node.pos.x;
-        }
-        if node.pos.x > max_x {
-            max_x = node.pos.x;
-        }
-        if node.pos.y < min_y {
-            min_y = node.pos.y;
-        }
-        if node.pos.y > max_y {
-            max_y = node.pos.y;
-        }
-    }
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
-            let pos = Pos { x, y };
-            if state.current.iter().find(|n| n.pos == pos).is_some() {
-                print!("@");
-            } else if grid.contains_key(&pos) {
-                print!("{}", grid.get(&pos).unwrap());
-            } else {
-                print!(" ");
-            }
-        }
-        println!();
-    }
-}
-
-fn display_state(grid: &Grid, state: &State2) {
+fn display_state(grid: &Grid, state: &State) {
     let mut min_x = i64::MAX;
     let mut max_x = i64::MIN;
     let mut min_y = i64::MAX;
@@ -476,8 +290,8 @@ fn display_state(grid: &Grid, state: &State2) {
 fn dijkstra(grid: &Grid, graph: &Graph, start_nodes: Vec<Node>) -> i32 {
     let number_of_keys = grid.number_of_keys();
     let mut frontier: BinaryHeap<StateCost> = BinaryHeap::new();
-    let mut cost_so_far: HashMap<State2, i32> = HashMap::new();
-    let start = State2 {
+    let mut cost_so_far: HashMap<State, i32> = HashMap::new();
+    let start = State {
         current: start_nodes.clone(),
         keys: Vec::new(),
     };
@@ -485,87 +299,32 @@ fn dijkstra(grid: &Grid, graph: &Graph, start_nodes: Vec<Node>) -> i32 {
         state: start.clone(),
         cost: 0,
     });
-    cost_so_far.insert(start.clone(), 0);
+    cost_so_far.insert(start, 0);
 
     while !frontier.is_empty() {
         let StateCost {
             state: current,
             cost,
         } = frontier.pop().unwrap();
-        // println!("pop {:?} {:?}", current, cost);
-        // display_state(&grid, &current);
-        // println!("cost_so_far {:?}", cost_so_far.get(&current));
         if current.keys.len() == number_of_keys {
             println!("Found all keys: {:?}", current);
             return cost_so_far[&current];
         }
-        for (next, cost) in neighbours2(graph, &current) {
+        for (next, cost) in neighbours(graph, &current) {
             let next_cost = cost_so_far.get(&next);
             let current_cost = cost_so_far.get(&current).unwrap();
             let new_cost = current_cost + cost;
-            // println!("next_cost {:?} new_cost {:?}", next_cost, new_cost);
             if next_cost.is_none() || new_cost < *next_cost.unwrap() {
                 cost_so_far.insert(next.clone(), new_cost);
                 frontier.push(StateCost {
-                    state: next.clone(),
-                    // cost: new_cost - next.keys.len() as i32,
+                    state: next,
                     cost: new_cost,
                 });
-                let len = frontier.len();
-                if len % 10000 == 0 {
-                    println!("len {:?}", len);
-                }
-                // println!("push {:?} {:?}", next, new_cost);
-                // println!("frontier {:?}", frontier);
-            } else {
-                // println!("skip {:?} {:?}", next_cost, new_cost);
             }
         }
     }
     0
 }
-
-// fn bfs2(graph: &Graph, start_nodes: Vec<Node>, number_of_keys: usize) -> i32 {
-//     let mut min_cost = i32::MAX;
-//     let mut queue = Vec::new();
-//     let mut visited: HashMap<(Vec<Node>, Vec<char>), i32> = HashMap::new();
-//     queue.push(State2 {
-//         current: start_nodes,
-//         keys: Vec::new(),
-//         cost: 0,
-//     });
-//
-//     while !queue.is_empty() {
-//         let state = queue.remove(0);
-//         // println!("{:?}", state);
-//         if visited.contains_key(&(state.current.clone(), state.keys.clone())) {
-//             let cost = visited
-//                 .get(&(state.current.clone(), state.keys.clone()))
-//                 .unwrap();
-//             if state.cost >= *cost {
-//                 // println!("skip {:?} {:?}", state, cost);
-//                 continue;
-//             }
-//         }
-//         visited.insert((state.current.clone(), state.keys.clone()), state.cost);
-//
-//         if state.keys.len() == number_of_keys {
-//             if state.cost < min_cost {
-//                 // println!("Found all keys: {:?}", state);
-//                 min_cost = state.cost;
-//             }
-//             continue;
-//         }
-//
-//         let neighboors = neighbours2(graph, &state);
-//         // println!("neighboors {:?}", neighboors);
-//         for neighboor in neighboors {
-//             // println!("add {:?}", neighboor);
-//             queue.push(neighboor);
-//         }
-//     }
-//     min_cost
-// }
 
 pub fn part2(input: String) -> i64 {
     let mut grid = build_grid(input);
@@ -593,11 +352,9 @@ pub fn part2(input: String) -> i64 {
             y: center.y + 1,
         },
     ];
-    for pos in start_positions.clone() {
-        grid.insert(pos, '@');
+    for pos in start_positions.iter() {
+        grid.insert(*pos, '@');
     }
-    // println!("graph {:?}", graph);
-    // grid.display();
 
     let start_nodes = start_positions
         .iter()
@@ -607,10 +364,8 @@ pub fn part2(input: String) -> i64 {
         })
         .collect::<Vec<Node>>();
     let mut graph = Graph::new();
-    for start in start_nodes.clone() {
-        extend_graph(&mut graph, &grid, &start);
+    for start in start_nodes.iter() {
+        extend_graph(&mut graph, &grid, start);
     }
-    // println!("graph {:?}", graph);
-    // bfs2(&graph, start_nodes, grid.number_of_keys()) as i64
     dijkstra(&grid, &graph, start_nodes) as i64
 }

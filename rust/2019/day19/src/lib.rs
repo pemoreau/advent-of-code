c699796757a6fac05c00f28a81db80868c4263f8
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-10-02 18:35:35 +0200

    part2

--- rust/2019/day19/src/lib.rs
+++ rust/2019/day19/src/lib.rs
@@ -48,18 +48,34 @@ pub fn part1(input: String) -> i64 {
 pub fn part2(input: String) -> i64 {
     let code = comma_separated_to_numbers(input);
     let mut grid = Grid::new();
-    let mut cpt = 0;
-    for y in 0..100 {
-        for x in 0..100 {
+    let size = 2500;
+    for y in 0..size {
+        for x in 0..size {
             let mut machine = Machine::new(code.clone(), vec![x, y]);
             machine.run();
             let output = machine.get_output();
-            grid.insert(Pos { x, y }, if output[0] == 0 { '.' } else { '#' });
             if output[0] == 1 {
-                cpt += 1;
+                grid.insert(Pos { x, y }, '#');
             }
         }
     }
-    display(&grid);
-    cpt
+    let square_size = 100;
+    for y in 0..size - square_size {
+        for x in 0..size - square_size {
+            if contains_square(&grid, x, y, square_size) {
+                return x * 10000 + y;
+            }
+        }
+    }
+    0
+}
+
+fn contains_square(grid: &Grid, x: i64, y: i64, size: i64) -> bool {
+    return grid.get(&Pos { x, y }) == Some(&'#')
+        && grid.get(&Pos { x: x + size - 1, y }) == Some(&'#')
+        && grid.get(&Pos { x, y: y + size - 1 }) == Some(&'#')
+        && grid.get(&Pos {
+            x: x + size - 1,
+            y: y + size - 1,
+        }) == Some(&'#');
 }

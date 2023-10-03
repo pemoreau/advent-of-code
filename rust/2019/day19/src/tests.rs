d26512e54cd48b95890eef2e057430eebc9dab8c
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-10-03 08:20:23 +0200

    part2

--- rust/2019/day19/src/tests.rs
+++ rust/2019/day19/src/tests.rs
@@ -3,11 +3,11 @@ mod tests {
 
     #[test]
     fn test_input_part1() {
-        assert_eq!(0, part1(include_str!("../input.txt").to_string()));
+        assert_eq!(150, part1(include_str!("../input.txt").to_string()));
     }
 
     #[test]
     fn test_input_part2() {
-        assert_eq!(0, part2(include_str!("../input.txt").to_string()));
+        assert_eq!(12201460, part2(include_str!("../input.txt").to_string()));
     }
 }

ceb3c0a14b3454e82c1e22790211c03ac5c5954e
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-12-06 07:01:55 +0100

    Day06

--- go/2023/06/day06.go
+++ go/2023/06/day06.go
@@ -3,22 +3,69 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
 //go:embed input.txt
 var inputDay string
 
+func buildArray(line string) []int {
+	var res []int
+	for _, field := range strings.Fields(line) {
+		if v, err := strconv.Atoi(strings.TrimSpace(field)); err == nil {
+			res = append(res, v)
+		}
+	}
+	return res
+}
+
+func buildConcat(line string) int {
+	s := ""
+	for _, field := range strings.Fields(line) {
+		v := strings.TrimSpace(field)
+		if _, err := strconv.Atoi(v); err == nil {
+			s = s + v
+		}
+	}
+	res, _ := strconv.Atoi(s)
+	return res
+}
+
+func calcDist(hold, maxTime int) int {
+	return (maxTime - hold) * hold
+}
+
+func solve(time, dist int) int {
+	res := 0
+	for i := 0; i < time; i++ {
+		d := calcDist(i, time)
+		if d > dist {
+			res++
+		}
+	}
+	return res
+}
+
 func Part1(input string) int {
-	//input = strings.TrimSuffix(input, "\n")
-	//lines := strings.Split(input, "\n")
-	return 0
+	input = strings.TrimSuffix(input, "\n")
+	lines := strings.Split(input, "\n")
+
+	var times = buildArray(lines[0])
+	var dist = buildArray(lines[1])
+
+	res := 1
+	for i, t := range times {
+		res *= solve(t, dist[i])
+	}
+	return res
 }
 
 func Part2(input string) int {
-	//input = strings.TrimSuffix(input, "\n")
-	//lines := strings.Split(input, "\n")
-	return 0
+	input = strings.TrimSuffix(input, "\n")
+	lines := strings.Split(input, "\n")
+	return solve(buildConcat(lines[0]), buildConcat(lines[1]))
 }
 
 func main() {
--- go/2023/06/day06_test.go
+++ go/2023/06/day06_test.go
@@ -10,35 +10,35 @@ var inputTest string
 
 func TestPart1(t *testing.T) {
 	result := Part1(inputTest)
-	expected := 35
+	expected := 288
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
 	}
 }
 
-//func TestPart2(t *testing.T) {
-//	result := Part2(inputTest)
-//	expected := 46
-//	if result != expected {
-//		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
-//	}
-//}
-//
-//func TestPart1Input(t *testing.T) {
-//	result := Part1(inputDay)
-//	expected := 157211394
-//	if result != expected {
-//		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
-//	}
-//}
-//
-//func TestPart2Input(t *testing.T) {
-//	result := Part2(inputDay)
-//	expected := 50855035
-//	if result != expected {
-//		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
-//	}
-//}
+func TestPart2(t *testing.T) {
+	result := Part2(inputTest)
+	expected := 71503
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart1Input(t *testing.T) {
+	result := Part1(inputDay)
+	expected := 1312850
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart2Input(t *testing.T) {
+	result := Part2(inputDay)
+	expected := 36749103
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
 
 func BenchmarkPart1(b *testing.B) {
 	for i := 0; i < b.N; i++ {

80990b224dddf093260cbad6ae95ecfc96ec508e
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-12-08 11:35:29 +0100

    day8

---
+++ go/2023/08/day08_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+)
+
+//go:embed input_test.txt
+var inputTest string
+
+//go:embed input_test2.txt
+var inputTest2 string
+
+func TestPart1(t *testing.T) {
+	result := Part1(inputTest)
+	expected := 6
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(inputTest2)
+	expected := 6
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart1Input(t *testing.T) {
+	result := Part1(inputDay)
+	expected := 12599
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart2Input(t *testing.T) {
+	result := Part2(inputDay)
+	expected := 8245452805243
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Part1(inputDay)
+	}
+}
+func BenchmarkPart2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Part2(inputDay)
+	}
+}

41df46868a26ffef574ed8b97ab43da0eb9f2e43
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-12-14 11:08:02 +0100

    Day12 with go routine

---
+++ go/2023/12Worker/day12_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+)
+
+//go:embed input_test.txt
+var inputTest string
+
+func TestPart1(t *testing.T) {
+	result := Part1(inputTest)
+	expected := 21
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(inputTest)
+	expected := 525152
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart1Input(t *testing.T) {
+	result := Part1(inputDay)
+	expected := 7379
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func TestPart2Input(t *testing.T) {
+	result := Part2(inputDay)
+	expected := 7732028747925
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Part1(inputDay)
+	}
+}
+func BenchmarkPart2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Part2(inputDay)
+	}
+}

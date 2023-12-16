3de934b7d00f265877f19dc700dacbf3abfe99ab
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-12-16 16:49:35 +0100

    day16

--- go/2023/16/day16_test.go
+++ go/2023/16/day16_test.go
@@ -31,13 +31,13 @@ func TestPart1Input(t *testing.T) {
 	}
 }
 
-//func TestPart2Input(t *testing.T) {
-//	result := Part2(inputDay)
-//	expected := 236358
-//	if result != expected {
-//		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
-//	}
-//}
+func TestPart2Input(t *testing.T) {
+	result := Part2(inputDay)
+	expected := 7530
+	if result != expected {
+		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+	}
+}
 
 func BenchmarkPart1(b *testing.B) {
 	for i := 0; i < b.N; i++ {

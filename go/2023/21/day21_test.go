88e7b80018b48006e85d20046300f87856ace3bc
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2023-12-21 19:12:15 +0100

    day 21

--- go/2023/21/day21_test.go
+++ go/2023/21/day21_test.go
@@ -8,13 +8,13 @@ import (
 //go:embed input_test.txt
 var inputTest string
 
-func TestPart1(t *testing.T) {
-	result := Part1(inputTest)
-	expected := 3605
-	if result != expected {
-		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
-	}
-}
+//func TestPart1(t *testing.T) {
+//	result := Part1(inputTest)
+//	expected := 3605
+//	if result != expected {
+//		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
+//	}
+//}
 
 //func TestPart2(t *testing.T) {
 //	result := Part2(inputTest)

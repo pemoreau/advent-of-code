c6a85a6403541240ede3fc09842b1a1a1d737ce7
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2024-12-06 16:09:33 +0100

    day 6

--- go/2024/06/day06.go
+++ go/2024/06/day06.go
@@ -4,29 +4,107 @@ import (
 	_ "embed"
 	"fmt"
 	"github.com/pemoreau/advent-of-code/go/utils"
+	"github.com/pemoreau/advent-of-code/go/utils/game2d"
+	"github.com/pemoreau/advent-of-code/go/utils/set"
 	"time"
 )
 
 //go:embed sample.txt
 var inputTest string
 
-func Part1(input string) int {
-	//var lines = strings.Split(input, "\n")
+const (
+	north = 0
+	east  = 1
+	south = 2
+	west  = 3
+)
 
-	var res int
-	return res
+type Guard struct {
+	game2d.Pos
+	dir int
+}
+
+func findStart(grid *game2d.GridChar) Guard {
+	for p, e := range grid.All() {
+		if e == '^' {
+			return Guard{p, north}
+		}
+	}
+	return Guard{}
+}
+
+func front(grid game2d.GridChar, guard Guard) (game2d.Pos, uint8, bool) {
+	var nextPos = []game2d.Pos{guard.Pos.N(), guard.Pos.E(), guard.Pos.S(), guard.Pos.W()}
+	v, inside := grid.GetPos(nextPos[guard.dir])
+	return nextPos[guard.dir], v, inside
+}
+
+func move(grid *game2d.GridChar, g Guard, path set.Set[Guard]) (guard Guard, inside bool, loop bool) {
+	if p, v, inside := front(*grid, g); !inside {
+		return g, false, false
+	} else if v != '#' && path.Contains(Guard{p, g.dir}) {
+		return g, true, true
+	} else if v == '#' {
+		return Guard{g.Pos, (g.dir + 1) % 4}, true, false
+	} else {
+		return Guard{p, g.dir}, true, false
+	}
+}
+
+func run(grid *game2d.GridChar, g Guard, path set.Set[Guard]) bool {
+	// returns true if loop
+	for {
+		var inside, loop bool
+		g, inside, loop = move(grid, g, path)
+		if loop {
+			return true
+		} else if !inside {
+			return false
+		}
+		path.Add(g)
+	}
+}
+
+func computeTrack(grid *game2d.GridChar, guard Guard, path set.Set[Guard]) set.Set[game2d.Pos] {
+	var plot = set.NewSet[game2d.Pos]()
+	run(grid, guard, path)
+	for p := range path.All() {
+		plot.Add(p.Pos)
+	}
+	return plot
+}
+
+func Part1(input string) int {
+	var grid = game2d.BuildGridCharFromString(input)
+	var path = set.NewSet[Guard]()
+	var guard = findStart(grid)
+	path.Add(guard)
+	return computeTrack(grid, guard, path).Len()
 }
 
 func Part2(input string) int {
-	//var lines = strings.Split(input, "\n")
+	var grid = game2d.BuildGridCharFromString(input)
+	var plot = set.NewSet[game2d.Pos]()
 
-	var res int
-	return res
+	var guard = findStart(grid)
+	var track = computeTrack(grid, guard, set.NewSet[Guard]())
+
+	for p := range track.All() {
+		if v, _ := grid.GetPos(p); v == '.' {
+			grid.SetPos(p, '#')
+			if loop := run(grid, guard, set.NewSet[Guard]()); loop {
+				plot.Add(p)
+			}
+			grid.SetPos(p, '.')
+		}
+	}
+	return plot.Len()
 }
 
 func main() {
 	fmt.Println("--2024 day 06 solution--")
 	var inputDay = utils.Input()
+	//var inputDay = inputTest
 	start := time.Now()
 	fmt.Println("part1: ", Part1(inputDay))
 	fmt.Println(time.Since(start))

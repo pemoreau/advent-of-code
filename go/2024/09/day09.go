bc952de052e9cc60b8f371cddecf626439d9a768
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2024-12-09 17:44:41 +0100

    day 9

--- go/2024/09/day09.go
+++ go/2024/09/day09.go
@@ -14,14 +14,16 @@ var inputTest string
 type file struct {
 	id  int
 	len int
-	adr int
+	//adr int
 }
 
 func (f file) String() string {
 	if f.id == -1 {
-		return fmt.Sprintf("{FREE, len: %d, adr: %d}", f.len, f.adr)
+		//return fmt.Sprintf("{FREE, len: %d, adr: %d}", f.len, f.adr)
+		return fmt.Sprintf("{FREE, len: %d}", f.len)
 	}
-	return fmt.Sprintf("{#%d, len: %d, adr: %d}", f.id, f.len, f.adr)
+	//return fmt.Sprintf("{#%d, len: %d, adr: %d}", f.id,f.len, f.adr)
+	return fmt.Sprintf("{#%d, len: %d}", f.id, f.len)
 }
 
 type block = []*file
@@ -33,10 +35,12 @@ func parse(input string) []block {
 		var d = int(c - '0')
 		var f file
 		if i%2 == 0 {
-			f = file{id, d, adr}
+			//f = file{id, d, adr}
+			f = file{id, d}
 			id++
 		} else {
-			f = file{-1, d, adr}
+			//f = file{-1, d, adr}
+			f = file{-1, d}
 		}
 		var b = block{&f}
 		blocks = append(blocks, b)
@@ -45,17 +49,20 @@ func parse(input string) []block {
 	return blocks
 }
 
-func findFreeBlock1(blocks []block, indexFree int) int {
+func findFreeBlock1(blocks []block, indexFree int, maxIndex int) int {
 	if indexFree < 0 || indexFree >= len(blocks) {
 		panic("indexFree out of range")
 	}
-	for i := indexFree; i < len(blocks); i++ {
+	for i := indexFree; i < maxIndex; i++ {
 		var b = blocks[i]
-		if len(b) > 0 && b[len(b)-1].id == -1 {
-			return i
+		if len(b) > 0 {
+			var last = b[len(b)-1]
+			if last.id == -1 {
+				return i
+			}
 		}
 	}
-	panic("no free block")
+	return -1
 }
 
 func findFreeBlock2(blocks []block, maxIndex int, size int) int {
@@ -91,9 +98,10 @@ func defragFile(blocks []block, indexFree int, f *file) bool {
 		return true
 	} else if f.len < freeFile.len {
 		// fill freeFile, split freeFile and make f free
-		var newFile = &file{f.id, f.len, freeFile.adr}
+		//var newFile = &file{f.id, f.len, freeFile.adr}
+		var newFile = &file{f.id, f.len}
 		freeFile.len -= f.len
-		freeFile.adr += f.len
+		//freeFile.adr += f.len
 		f.id = -1
 		// insert f in freeBlock before last position
 		freeBlock = append(freeBlock[:len(freeBlock)-1], newFile, freeBlock[len(freeBlock)-1])
@@ -109,6 +117,10 @@ func checksum(blocks []block) int {
 	for _, b := range blocks {
 		for _, f := range b {
 			if f.id != -1 {
+				//var sum = (f.adr + f.len - 1) * (f.adr + f.len - 1 + 1) / 2
+				//var start = (f.adr - 1) * (f.adr) / 2
+				//res += f.id * (sum - start)
+
 				for i := 0; i < f.len; i++ {
 					res += adr * f.id
 					adr++
@@ -121,7 +133,7 @@ func checksum(blocks []block) int {
 	return res
 }
 
-func Part1(input string) int {
+func solve(input string, part2 bool) int {
 	input = strings.TrimSpace(input)
 	var blocks = parse(input)
 
@@ -130,42 +142,35 @@ func Part1(input string) int {
 		lastIndex--
 	}
 
-	var freeIndex = 1
-	for {
-		var f = blocks[lastIndex][0]
-		ok := defragFile(blocks, freeIndex, f)
-		if !ok {
-			break
+	if !part2 {
+		for freeIndex := 1; freeIndex >= 0; {
+			var f = blocks[lastIndex][0]
+			if ok := defragFile(blocks, freeIndex, f); !ok {
+				break
+			}
+			freeIndex = findFreeBlock1(blocks, freeIndex, lastIndex)
+			if f.id == -1 {
+				lastIndex -= 2
+			}
 		}
-		freeIndex = findFreeBlock1(blocks, freeIndex)
-		if freeIndex < 0 || freeIndex >= lastIndex {
-			break
-		}
-		if f.id == -1 {
+	} else {
+		for lastIndex >= 1 {
+			var f = blocks[lastIndex][0]
+			if indexFree := findFreeBlock2(blocks, lastIndex, f.len); indexFree >= 0 {
+				defragFile(blocks, indexFree, f)
+			}
 			lastIndex -= 2
 		}
 	}
 	return checksum(blocks)
 }
 
+func Part1(input string) int {
+	return solve(input, false)
+}
+
 func Part2(input string) int {
-	input = strings.TrimSpace(input)
-	var blocks = parse(input)
-
-	var lastIndex = len(blocks) - 1
-	if lastIndex%2 != 0 {
-		lastIndex--
-	}
-
-	for lastIndex >= 1 {
-		var f = blocks[lastIndex][0]
-		var indexFree = findFreeBlock2(blocks, lastIndex, f.len)
-		if indexFree >= 0 {
-			defragFile(blocks, indexFree, f)
-		}
-		lastIndex -= 2
-	}
-	return checksum(blocks)
+	return solve(input, true)
 }
 
 func main() {

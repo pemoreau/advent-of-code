856a94460c0bcb8cec5596078729614024060852
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2024-12-11 07:25:23 +0100

    day 11

--- go/2024/11/day11.go
+++ go/2024/11/day11.go
@@ -3,30 +3,73 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"github.com/pemoreau/advent-of-code/go/utils"
+	"strconv"
+	"strings"
 	"time"
 )
 
 //go:embed sample.txt
 var inputTest string
 
-func Part1(input string) int {
-	//var lines = strings.Split(input, "\n")
+type config struct {
+	v int
+	n int
+}
 
-	var res int
+var cache = make(map[config]int)
+
+func produce(value int, n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	if r, ok := cache[config{value, n}]; ok {
+		return r
+	}
+
+	if value == 0 {
+		res := produce(1, n-1)
+		cache[config{value, n}] = res
+		return res
+	}
+
+	var s = strconv.Itoa(value)
+	if len(s)%2 == 0 {
+		a, _ := strconv.Atoi(s[:len(s)/2])
+		b, _ := strconv.Atoi(s[len(s)/2:])
+		res := produce(a, n-1) + produce(b, n-1)
+		cache[config{value, n}] = res
+		return res
+	}
+
+	res := produce(value*2024, n-1)
+	cache[config{value, n}] = res
 	return res
+}
+
+func solve(input string, n int) int {
+	var line = strings.Split(input, " ")
+	var res int
+	for _, e := range line {
+		v, _ := strconv.Atoi(e)
+		res += produce(v, n)
+	}
+	return res
+}
+
+func Part1(input string) int {
+	return solve(input, 25)
 }
 
 func Part2(input string) int {
-	//var lines = strings.Split(input, "\n")
-
-	var res int
-	return res
+	return solve(input, 75)
 }
 
 func main() {
-	fmt.Println("--2024 day 10 solution--")
-	//var inputDay = utils.Input()
-	var inputDay = inputTest
+	fmt.Println("--2024 day 11 solution--")
+	var inputDay = utils.Input()
+	//var inputDay = inputTest
 	start := time.Now()
 	fmt.Println("part1: ", Part1(inputDay))
 	fmt.Println(time.Since(start))

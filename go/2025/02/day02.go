dc7dc61203d28afbcabb3c71b808fb0489158927
pem <Pierre-Etienne.Moreau@univ-lorraine.fr>
2025-12-02 13:49:27 +0100

    day2

--- go/2025/02/day02.go
+++ go/2025/02/day02.go
@@ -3,22 +3,102 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"math"
+	"strings"
 	"time"
 
 	"github.com/pemoreau/advent-of-code/go/utils"
+	"github.com/pemoreau/advent-of-code/go/utils/interval"
+	"github.com/pemoreau/advent-of-code/go/utils/set"
 )
 
+func isValid(n int, list []interval.Interval) bool {
+	for _, i := range list {
+		if i.Contains(n) {
+			return true
+		}
+	}
+	return false
+}
+
+func parse(input string) ([]interval.Interval, int) {
+	var ranges = strings.Split(input, ",")
+	var intervals []interval.Interval
+	var maxB int
+	for _, r := range ranges {
+		var a, b int
+		_, _ = fmt.Sscanf(r, "%d-%d", &a, &b)
+		intervals = append(intervals, interval.Interval{a, b})
+		maxB = max(maxB, b)
+	}
+	return intervals, maxB
+}
+
+func shiftLeft(number int, n int) int {
+	for range n {
+		number *= 10
+	}
+	return number
+}
+
+func nbDigit(number int) int {
+	if number == 0 {
+		return 1
+	}
+	return 1 + int(math.Log10(float64(number)))
+}
+
+func concat(a, b int) int {
+	return shiftLeft(a, nbDigit(b)) + b
+}
+
+func repeat(number int, maxDigit int) []int {
+	var res []int
+	var n = number
+	var nbDigitNumber = nbDigit(number)
+	var nbDigit = nbDigitNumber
+	for nbDigit <= maxDigit {
+		n = concat(n, number)
+		nbDigit += nbDigitNumber
+		if nbDigit <= maxDigit {
+			res = append(res, n)
+		}
+	}
+	return res
+}
+
 func Part1(input string) int {
-	//input = strings.TrimSuffix(input, "\n")
-	//var lines = strings.Split(input, "\n")
+	intervals, maxB := parse(input)
+	var maxDigit = nbDigit(maxB)
+
 	var res int
+	var upper = shiftLeft(1, maxDigit/2)
+	for i := range upper {
+		if n := concat(i, i); isValid(n, intervals) {
+			res = res + n
+		}
+	}
 	return res
 }
 
 func Part2(input string) int {
-	//input = strings.TrimSuffix(input, "\n")
-	//var lines = strings.Split(input, "\n")
+	intervals, maxB := parse(input)
+	var maxDigit = nbDigit(maxB)
+
+	var upper = shiftLeft(1, maxDigit/2)
+	var numbers = set.NewSet[int]()
+	for i := range upper {
+		var l = repeat(i, maxDigit)
+		numbers.AddAll(l...)
+	}
+
 	var res int
+	for i := range numbers {
+		if isValid(i, intervals) {
+			res += i
+		}
+	}
+
 	return res
 }
 
--- go/2025/02/day02_test.go
+++ go/2025/02/day02_test.go
@@ -10,20 +10,17 @@ import (
 func TestPart(t *testing.T) {
 	var tests = []utils.Test[string, int]{
 		{
-			Func: Part1,
-			Input: utils.Dedent(`
-			`),
-			Expected: 0,
+			Func:     Part1,
+			Input:    "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",
+			Expected: 1227775554,
 		},
-		//{
-		//	Func: Part2,
-		//	Input: utils.Dedent(`
-		//	`),
-		//	Expected: 0,
-		//},
-		//
-		//{Func: Part1, Input: utils.Input(), Expected: 0},
-		//{Func: Part2, Input: utils.Input(), Expected: 0},
+		{
+			Func:     Part2,
+			Input:    "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",
+			Expected: 4174379265,
+		},
+		{Func: Part1, Input: utils.Input(), Expected: 31210613313},
+		{Func: Part2, Input: utils.Input(), Expected: 41823587546},
 	}
 	utils.TestPart(t, tests)
 }
